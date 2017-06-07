a31ffd568c310324a1c660cab6fc4c5613427245
clion007 <clion007@126.com>
2017-06-07 14:58:39 +0800

    Update dnsmasq_fqad.sh

--- dnsmasq_fqad.sh
+++ dnsmasq_fqad.sh
@@ -45,32 +45,38 @@ nameserver 223.5.5.5
 nameserver 119.29.29.29
 EOF
 
-# 下载扶墙和广告规则
+# 开始下载扶墙和广告规则
+
 # 下载dnsmasq规则
-# 下载sy618扶墙规则
-wget --no-check-certificate -qO - https://raw.githubusercontent.com/sy618/hosts/master/dnsmasq/dnsfq -O /etc/dnsmasq.d/fq.conf
+# # 下载sy618规则
+wget --no-check-certificate -qO - https://raw.githubusercontent.com/sy618/hosts/master/dnsmasq/dnsfq > /etc/dnsmasq.d/sy168.conf
+# 下载racaljk规则
+wget --no-check-certificate -qO - https://raw.githubusercontent.com/racaljk/hosts/master/dnsmasq.conf > /etc/dnsmasq.d/racaljk.conf
 # 下载vokins广告规则
-wget --no-check-certificate -qO - https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/union.conf -O /etc/dnsmasq.d/ad.conf
+wget --no-check-certificate -qO - https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/union.conf > /etc/dnsmasq.d/ad.conf
 # 下载easylistchina广告规则
-wget --no-check-certificate -qO - https://c.nnjsx.cn/GL/dnsmasq/update/adblock/easylistchina.txt -O /etc/dnsmasq.d/easylistchina.conf
+wget --no-check-certificate -qO - https://c.nnjsx.cn/GL/dnsmasq/update/adblock/easylistchina.txt > /etc/dnsmasq.d/easylistchina.conf
 # 合并dnsmasq缓存
-cd /etc/dnsmasq.d;cat fq.conf ad.conf easylistchina.conf > fqad
+cd /etc/dnsmasq.d;cat racaljk.conf sy168.conf ad.conf easylistchina.conf > fqad
 # 删除dnsmasq缓存
 rm -rf /etc/dnsmasq.d/ad.conf
-rm -rf /etc/dnsmasq.d/fq.conf
+rm -rf /etc/dnsmasq.d/sy168.conf
+rm -rf /etc/dnsmasq.d/racaljk.conf
 rm -rf /etc/dnsmasq.d/easylistchina.conf
 # 删除dnsmasq重复规则
 sort /etc/dnsmasq.d/fqad | uniq > /etc/dnsmasq.d/fqad.conf
 # 删除dnsmasq合并缓存
 rm -rf /etc/dnsmasq.d/fqad
+# 删除无用的注释
+sed -i '/#/d' /etc/dnsmasq.d/fqad.conf
 
 # 下载hosts规则
 # 下载yhosts缓存
-wget --no-check-certificate -qO - https://raw.githubusercontent.com/vokins/yhosts/master/hosts.txt -O /etc/dnsmasq/yhosts.conf
+wget --no-check-certificate -qO - https://raw.githubusercontent.com/vokins/yhosts/master/hosts.txt > /etc/dnsmasq/yhosts.conf
 # 下载malwaredomainlist?嬖?
-wget --no-check-certificate -qO - http://www.malwaredomainlist.com/hostslist/hosts.txt -O /etc/dnsmasq/malwaredomainlist.conf
+wget --no-check-certificate -qO - http://www.malwaredomainlist.com/hostslist/hosts.txt > /etc/dnsmasq/malwaredomainlist.conf
 # 下载adaway规则缓存
-wget --no-check-certificate -qO - http://77l5b4.com1.z0.glb.clouddn.com/hosts.txt -O /etc/dnsmasq/adaway.conf
+wget --no-check-certificate -qO - http://77l5b4.com1.z0.glb.clouddn.com/hosts.txt > /etc/dnsmasq/adaway.conf
 # 合并hosts缓存
 cd /etc/dnsmasq;cat yhosts.conf adaway.conf malwaredomainlist.conf > noad
 # 删除hosts缓存
@@ -81,10 +87,13 @@ rm -rf /etc/dnsmasq/malwaredomainlist.conf
 sort /etc/dnsmasq/noad | uniq > /etc/dnsmasq/noad.conf
 # 删除hosts合并缓存
 rm -rf /etc/dnsmasq/noad
+# 删除无用的注释
+sed -i '/#/d' /etc/dnsmasq/noad.conf
 
 # 重启dnsmasq服务
 killall dnsmasq
 /usr/sbin/dnsmasq
+# 扶墙和广告屏蔽规则下载结束
 
 # 创建规则更新脚本
 cat > /etc/dnsmasq/fqad_update.sh <<EOF
@@ -92,31 +101,37 @@ cat > /etc/dnsmasq/fqad_update.sh <<EOF
 # 移动到用户命令文件夹
 cd /usr/bin/
 
-# 下载dnsmasq规则
-# 下载sy618扶墙规则
-wget --no-check-certificate -qO - https://raw.githubusercontent.com/sy618/hosts/master/dnsmasq/dnsfq -O /etc/dnsmasq.d/fq.conf
+# 开始更新dnsmasq规则
+# # 下载sy618规则
+wget --no-check-certificate -qO - https://raw.githubusercontent.com/sy618/hosts/master/dnsmasq/dnsfq > /etc/dnsmasq.d/sy168.conf
+# 下载racaljk规则
+wget --no-check-certificate -qO - https://raw.githubusercontent.com/racaljk/hosts/master/dnsmasq.conf > /etc/dnsmasq.d/racaljk.conf
 # 下载vokins广告规则
-wget --no-check-certificate -qO - https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/union.conf -O /etc/dnsmasq.d/ad.conf
+wget --no-check-certificate -qO - https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/union.conf > /etc/dnsmasq.d/ad.conf
 # 下载easylistchina广告规则
-wget --no-check-certificate -qO - https://c.nnjsx.cn/GL/dnsmasq/update/adblock/easylistchina.txt -O /etc/dnsmasq.d/easylistchina.conf
+wget --no-check-certificate -qO - https://c.nnjsx.cn/GL/dnsmasq/update/adblock/easylistchina.txt > /etc/dnsmasq.d/easylistchina.conf
 # 合并dnsmasq缓存
-cd /etc/dnsmasq.d;cat fq.conf ad.conf easylistchina.conf > fqad
+cd /etc/dnsmasq.d;cat racaljk.conf sy168.conf ad.conf easylistchina.conf > fqad
 # 删除dnsmasq缓存
 rm -rf /etc/dnsmasq.d/ad.conf
-rm -rf /etc/dnsmasq.d/fq.conf
+rm -rf /etc/dnsmasq.d/sy168.conf
+rm -rf /etc/dnsmasq.d/racaljk.conf
 rm -rf /etc/dnsmasq.d/easylistchina.conf
 # 删除dnsmasq重复规则
 sort /etc/dnsmasq.d/fqad | uniq > /etc/dnsmasq.d/fqad.conf
 # 删除dnsmasq合并缓存
 rm -rf /etc/dnsmasq.d/fqad
+# 删除无用的注释
+sed -i '/#/d' /etc/dnsmasq.d/fqad.conf
+# dnsmasq更新规则结束
 
-# 下载hosts规则
+# 开始更新hosts规则
 # 下载yhosts缓存
-wget --no-check-certificate -qO - https://raw.githubusercontent.com/vokins/yhosts/master/hosts.txt -O /etc/dnsmasq/yhosts.conf
-# 下载malwaredomainlist规则
-wget --no-check-certificate -qO - http://www.malwaredomainlist.com/hostslist/hosts.txt -O /etc/dnsmasq/malwaredomainlist.conf
+wget --no-check-certificate -qO - https://raw.githubusercontent.com/vokins/yhosts/master/hosts.txt > /etc/dnsmasq/yhosts.conf
+# 下载malwaredomainlist?嬖?
+wget --no-check-certificate -qO - http://www.malwaredomainlist.com/hostslist/hosts.txt > /etc/dnsmasq/malwaredomainlist.conf
 # 下载adaway规则缓存
-wget --no-check-certificate -qO - http://77l5b4.com1.z0.glb.clouddn.com/hosts.txt -O /etc/dnsmasq/adaway.conf
+wget --no-check-certificate -qO - http://77l5b4.com1.z0.glb.clouddn.com/hosts.txt > /etc/dnsmasq/adaway.conf
 # 合并hosts缓存
 cd /etc/dnsmasq;cat yhosts.conf adaway.conf malwaredomainlist.conf > noad
 # 删除hosts缓存
@@ -127,6 +142,9 @@ rm -rf /etc/dnsmasq/malwaredomainlist.conf
 sort /etc/dnsmasq/noad | uniq > /etc/dnsmasq/noad.conf
 # 删除hosts合并缓存
 rm -rf /etc/dnsmasq/noad
+# 删除无用的注释
+sed -i '/#/d' /etc/dnsmasq/noad.conf
+# hosts更新规则结束
 
 # 重启dnsmasq服务
 killall dnsmasq
