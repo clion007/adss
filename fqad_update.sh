6eab609d9aa660b599f6e3cb66d4da77a284532f
clion007 <clion007@126.com>
2017-06-11 23:48:15 +0800

    Create fqad_update.sh

--- fqad_update.sh
+++ fqad_update.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+echo
 # Copyright (c) 2014-2017,by clion007
 # 本脚本仅用于个人研究与学习使用，从未用于产生任何盈利（包括“捐赠”等方式）
 # 未经许可，请勿内置于软件内发布与传播！请勿用于产生盈利活动！请遵守当地法律法规，文明上网。
@@ -8,6 +9,7 @@
 #if [ $LOGSIZE -ge 5000 ]; then
 #	sed -i -e 1,10d $LOGFILE
 #fi
+
 # 更新dnsmasq规则
 # 下载sy618扶墙规则
 /usr/bin/wget-ssl --no-check-certificate -q -O /tmp/sy618.conf https://raw.githubusercontent.com/sy618/hosts/master/dnsmasq/dnsfq
@@ -37,8 +39,9 @@ sed -i '/localhost/d' /tmp/fqad
 sed -i '/::1/d' /tmp/fqad
 # 创建dnsmasq规则文件
 cat > /tmp/fqad.conf <<EOF
-####################################################################
+############################################################
 ##【Copyright (c) 2014-2017, clion007】                           ##
+##                                                                ##
 ## 感谢https://github.com/sy618/hosts                             ##
 ## 感谢https://github.com/racaljk/hosts                           ##
 ####################################################################
@@ -52,6 +55,7 @@ address=/ip6-loopback/::1
 
 #Modified hosts start
 EOF
+
 # 删除dnsmasq重复规则
 sort /tmp/fqad | uniq >> /tmp/fqad.conf
 # 删除dnsmasq合并缓存
@@ -79,8 +83,9 @@ sed -i '/@/d' /tmp/noad
 sed -i '/::1/d' /tmp/noad
 sed -i '/localhost/d' /tmp/noad
 cat > /tmp/noad.conf <<EOF
-####################################################################
+############################################################
 ##【Copyright (c) 2014-2017, clion007】                           ##
+##                                                                ##
 ## 感谢https://github.com/sy618/hosts                             ##
 ## 感谢https://github.com/vokins/hosts                            ##
 ## 感谢https://github.com/racaljk/hosts                           ##
@@ -95,30 +100,34 @@ cat > /tmp/noad.conf <<EOF
 
 #修饰hosts开始
 EOF
+
 # 删除hosts重复规则
 sort /tmp/noad | uniq >> /tmp/noad.conf
 # 删除hosts合并缓存
 rm -rf /tmp/noad
 
-if [ -s "/tmp/fqad.conf" ];then
-	if ( ! cmp -s /tmp/fqad.conf /etc/dnsmasq.d/fqad.conf );then
+if [ -s "/tmp/fqad.conf" ]; then
+	if ( ! cmp -s /tmp/fqad.conf /etc/dnsmasq.d/fqad.conf ); then
 		mv /tmp/fqad.conf /etc/dnsmasq.d/fqad.conf
-		echo "$(date "+%F %T"):检测到fqad规则有更新......开始转换规则！"
+		echo "`date +'%Y-%m-%d %H:%M:%S'`:检测到fqad规则有更新......开始转换规则！"
 		/etc/init.d/dnsmasq restart >/dev/null 2>&1
-		echo "$(date "+%F %T"): fqad规则转换完成，应用新规则。"
+		echo "`date +'%Y-%m-%d %H:%M:%S'`: fqad规则转换完成，应用新规则。"
 		else
-		echo "$(date "+%F %T"): fqad本地规则和在线规则相同，无需更新!" && rm -f /tmp/fqad.conf
+		echo "`date +'%Y-%m-%d %H:%M:%S'`: fqad本地规则和在线规则相同，无需更新！" && rm -f /tmp/fqad.conf
 	fi	
 fi
-if [ -s "/tmp/noad.conf" ];then
-	if ( ! cmp -s /tmp/noad.conf /etc/dnsmasq/noad.conf );then
+echo
+if [ -s "/tmp/noad.conf" ]; then
+	if ( ! cmp -s /tmp/noad.conf /etc/dnsmasq/noad.conf ); then
 		mv /tmp/noad.conf /etc/dnsmasq/noad.conf
-		echo "$(date "+%F %T"): 检测到noad规则有更新......开始转换规则！"
+		echo "`date +'%Y-%m-%d %H:%M:%S'`: 检测到noad规则有更新......开始转换规则！"
 		/etc/init.d/dnsmasq restart >/dev/null 2>&1
-		echo "$(date "+%F %T"): noad规则转换完成，应用新规则。"
+		echo "`date +'%Y-%m-%d %H:%M:%S'`: noad规则转换完成，应用新规则。"
 		else
-		echo "$(date "+%F %T"): noad本地规则和在线规则相同，无需更新!" && rm -f /tmp/noad.conf
+		echo "`date +'%Y-%m-%d %H:%M:%S'`: noad本地规则和在线规则相同，无需更新！" && rm -f /tmp/noad.conf
 	fi	
 fi
-# 规则更新结束
+echo
+echo -e "\e[1;36m规则更新完成\e[0m"
+echo
 exit 0
