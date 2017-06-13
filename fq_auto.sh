a797dbabf175a2547826ebeb7389991379b4ab7a
clion007 <clion007@126.com>
2017-06-13 18:46:59 +0800

    Create fq_auto.sh

--- fq_auto.sh
+++ fq_auto.sh
@@ -19,7 +19,7 @@ echo "+                                                          +"
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo
 echo "------------------------------------------------------------------"
-echo -e "\e[1;31m请先查询你的\e[1;36mlan网关ip\e[1;31m再选择,\e[1;36mlan网关ip\e[1;31m必须为：\e[1;36m'192.168.1.1'\e[0m"
+echo -e "\e[1;31m 请先查询你的\e[1;36mlan网关ip\e[1;31m再选择,\e[1;36mlan网关ip\e[1;31m必须为：\e[1;36m'192.168.1.1'\e[0m"
 echo "------------------------------------------------------------------"
 echo
 echo -e "\e[1;36m >         1. 安装 \e[0m"
@@ -28,38 +28,38 @@ echo -e "\e[1;31m >         2. 卸载 \e[0m"
 echo
 echo -e "\e[1;36m >         3. 退出 \e[0m"
 echo
-echo -e -n "\e[1;34m请输入数字继续执行: \e[0m" 
+echo -e -n "\e[1;34m 请输入数字继续执行: \e[0m" 
 read menu
 if [ "$menu" == "1" ]; then
 echo
-echo -e "\e[1;36m三秒后开始安装......\e[0m"
+echo -e "\e[1;36m 三秒后开始安装......\e[0m"
 echo
 sleep 3
-echo -e "\e[1;36m正在更新软件包，根据网络状态决定时长\e[0m"
+echo -e "\e[1;36m 正在更新软件包，根据网络状态决定时长\e[0m"
 rm -f /var/lock/opkg.lock
 opkg update
 sleep 2
 echo
-echo -e "\e[1;36m开始检查并安装wget-支持https\e[0m"
+echo -e "\e[1;36m 开始检查并安装wget-支持https\e[0m"
 if [ -f $wgetroute ]; then
-	echo -e "\e[1;31m系统已经安装wget-ssl软件\e[0m"
+	echo -e "\e[1;31m 系统已经安装wget-ssl软件\e[0m"
 	#opkg remove wget > /dev/null 2>&1
 	#opkg install wget	
 	else
-	echo -e "\e[1;31m没有发现wget-ssl开始安装\e[0m"
+	echo -e "\e[1;31m 没有发现wget-ssl开始安装\e[0m"
 	opkg install wget
 	echo
 	if [ -f $wgetroute ]; then
-		echo -e "\e[1;36mwget安装成功         \e[0m[\e[1;31mmwget has been installde successfully\e[0m]"
+		echo -e "\e[1;36m wget安装成功         \e[0m[\e[1;31mmwget has been installde successfully\e[0m]"
 		else
-		echo -e "\e[1;31mwget安装失败,请到路由器系统软件包手动安装后再试!\e[0m"
+		echo -e "\e[1;31m wget安装失败,请到路由器系统软件包手动安装后再试!\e[0m"
 		exit
 	fi	
 fi
 echo
 sleep 3
 echo
-echo -e "\e[1;36m创建广告规则与更新脚本存放的文件夹\e[0m"
+echo -e "\e[1;36m 创建广告规则与更新脚本存放的文件夹\e[0m"
 if [ -f /etc/dnsmasq ]; then
 	mv /etc/dnsmasq /etc/dnsmasq.bak
 fi
@@ -72,7 +72,7 @@ mkdir -p /etc/dnsmasq
 mkdir -p /etc/dnsmasq.d
 sleep 3
 echo
-echo -e "\e[1;36mdnsmasq.conf 添加广告规则路径\e[0m"
+echo -e "\e[1;36m dnsmasq.conf 添加广告规则路径\e[0m"
 if [ -f /etc/dnsmasq.conf ]; then
 	mv /etc/dnsmasq.conf /etc/dnsmasq.conf.bak
 fi
@@ -96,7 +96,7 @@ conf-file=/etc/dnsmasq.d/fq.conf
 cache-size=10000" > /etc/dnsmasq.conf # 换成echo的方式注入
 echo
 sleep 3
-echo -e "\e[1;36m创建上游DNS配置文件\e[0m"
+echo -e "\e[1;36m 创建上游DNS配置文件\e[0m"
 cp /tmp/resolv.conf.auto /etc/dnsmasq/resolv.conf
 echo
 echo "# 上游DNS解析服务器
@@ -113,44 +113,44 @@ nameserver 114.114.114.114
 #nameserver 114.114.114.119" >> /etc/dnsmasq/resolv.conf # 换成echo的方式注入
 echo
 sleep 3
-echo -e -n "\e[1;36m创建自定义扶墙规则\e[0m"
+echo -e -n "\e[1;36m 创建自定义扶墙规则\e[0m"
 echo
 echo "# 规则格式,删除address前 # 生效，如有需要自己添加的规则，请打开userlist添加
 # 后面的地址有两种情况,优选具体ip地址
 #address=/.001union.com/127.0.0.1
 #address=/telegram.org/149.154.167.99" > /etc/dnsmasq.d/userlist
 echo
-echo -e "\e[1;36m下载扶墙规则\e[0m"
+echo -e "\e[1;36m 下载扶墙规则\e[0m"
 echo
-echo -e "\e[1;36m下载sy618扶墙规则\e[0m"
+echo -e "\e[1;36m 下载sy618扶墙规则\e[0m"
 /usr/bin/wget-ssl --no-check-certificate -q -O /tmp/sy618 https://raw.githubusercontent.com/sy618/hosts/master/dnsmasq/dnsfq
 echo
-echo -e "\e[1;36m下载racaljk规则\e[0m"
+echo -e "\e[1;36m 下载racaljk规则\e[0m"
 /usr/bin/wget-ssl --no-check-certificate -q -O /tmp/racaljk https://raw.githubusercontent.com/racaljk/hosts/master/dnsmasq.conf
 echo
 sleep 3
-#echo -e "\e[1;36m删除racaljk规则中google'youtube相关规则\e[0m"
+#echo -e "\e[1;36m 删除racaljk规则中google'youtube相关规则\e[0m"
 #sed -i '/google/d' /tmp/racaljk
 #sed -i '/youtube/d' /tmp/racaljk
 #echo
-echo -e "\e[1;36创建用户自定规则缓存\e[0m"
+echo -e "\e[1;36m 创建用户自定规则缓存\e[0m"
 cp /etc/dnsmasq.d/userlist /tmp/userlist
 echo
-echo -e -n "\e[1;36m删除dnsmasq缓存注释\e[0m"
+echo -e -n "\e[1;36m 删除dnsmasq缓存注释\e[0m"
 sed -i '/#/d' /tmp/sy618
 sed -i '/#/d' /tmp/racaljk
 sed -i '/#/d' /tmp/userlist
 echo
-echo -e -n "\e[1;36m扶墙网站指定到#443端口访问\e[0m"
+echo -e -n "\e[1;36m 扶墙网站指定到#443端口访问\e[0m"
 awk '{print $0"#443"}' /tmp/sy618 > /tmp/sy618.conf
 awk '{print $0"#443"}' /tmp/racaljk > /tmp/racaljk.conf
 awk '{print $0"#443"}' /tmp/userlist > /tmp/userlist.conf
 echo
-echo -e -n "\e[1;36m合并dnsmasq缓存\e[0m" 
+echo -e -n "\e[1;36m 合并dnsmasq缓存\e[0m" 
 cat /tmp/userlist.conf /tmp/racaljk.conf /tmp/sy618.conf > /tmp/fq
 #cat /tmp/userlist.conf /tmp/sy618.conf > /tmp/fq
 echo
-echo -e -n "\e[1;36m删除dnsmasq临时文件\e[0m"
+echo -e -n "\e[1;36m 删除dnsmasq临时文件\e[0m"
 rm -rf /tmp/userlist
 rm -rf /tmp/userlist.conf
 rm -rf /tmp/sy618.conf
@@ -158,11 +158,11 @@ rm -rf /tmp/sy618
 rm -rf /tmp/racaljk.conf
 rm -rf /tmp/racaljk
 echo
-echo -e "\e[1;36m删除本地规则\e[0m"
+echo -e "\e[1;36m 删除本地规则\e[0m"
 sed -i '/::1/d' /tmp/fq
 sed -i '/localhost/d' /tmp/fq
 echo
-echo -e "\e[1;36m创建dnsmasq规则文件\e[0m"
+echo -e "\e[1;36m 创建dnsmasq规则文件\e[0m"
 echo "
 ############################################################
 ##【Copyright (c) 2014-2017, clion007】                           ##
@@ -181,12 +181,12 @@ address=/ip6-loopback/::1
 #Modified hosts start
 " > /etc/dnsmasq.d/fq.conf # 换成echo的方式注入
 echo
-echo -e "\e[1;36m删除dnsmasq重复规则及相关临时文件\e[0m"
+echo -e "\e[1;36m 删除dnsmasq重复规则及相关临时文件\e[0m"
 sort /tmp/fq | uniq >> /etc/dnsmasq.d/fq.conf
 rm -rf /tmp/fq
 echo
 sleep 3
-echo -e "\e[1;36m重启dnsmasq服务\e[0m"
+echo -e "\e[1;36m 重启dnsmasq服务\e[0m"
 #killall dnsmasq
 	/etc/init.d/dnsmasq restart >/dev/null 2>&1
 echo
@@ -195,7 +195,7 @@ echo -e "\e[1;36m 获取规则更新脚本\e[0m"
 wget --no-check-certificate -q -O /etc/dnsmasq/fqad_update.sh https://raw.githubusercontent.com/clion007/dnsmasq/master/fq_update.sh
 echo
 sleep 1
-echo -e "\e[1;31m添加计划任务\e[0m"
+echo -e "\e[1;31m 添加计划任务\e[0m"
 chmod 755 /etc/dnsmasq/fq_update.sh
 sed -i '/dnsmasq/d' $CRON_FILE
 sed -i '/@/d' $CRON_FILE
@@ -204,7 +204,7 @@ echo "[$USER@$HOSTNAME:/$USER]#cat /etc/crontabs/$USER
 # 每天6点28分更新dnsmasq扶墙规则
 28 6 * * * /bin/sh /etc/dnsmasq/fq_update.sh > /dev/null 2>&1" >> $CRON_FILE
 /etc/init.d/cron reload
-echo -e "\e[1;36m定时计划任务添加完成！\e[0m"
+echo -e "\e[1;36m 定时计划任务添加完成！\e[0m"
 sleep 1
 echo
 echo
@@ -223,11 +223,11 @@ fi
 echo
 if [ "$menu" == "2" ]; then
 echo
-echo -e "\e[1;31m开始卸载dnsmasq扶墙及广告规则\e[0m"
+echo -e "\e[1;31m 开始卸载dnsmasq扶墙及广告规则\e[0m"
 	rm -f /var/lock/opkg.lock
 sleep 1
 echo
-echo -e "\e[1;31m删除残留文件夹以及配置\e[0m"
+echo -e "\e[1;31m 删除残留文件夹以及配置\e[0m"
 	rm -rf /etc/dnsmasq
 	rm -rf /etc/dnsmasq.d
 if [ -f /etc/dnsmasq.bak ]; then
@@ -244,12 +244,12 @@ if [ -f /etc/dnsmasq.conf.bak ]; then
 fi
 echo
 sleep 1
-echo -e "\e[1;31m删除相关计划任务\e[0m"
+echo -e "\e[1;31m 删除相关计划任务\e[0m"
 sed -i '/dnsmasq/d' $CRON_FILE
 /etc/init.d/cron reload
 sleep 1
 echo
-echo -e "\e[1;31m重启dnsmasq\e[0m"
+echo -e "\e[1;31m 重启dnsmasq\e[0m"
 	/etc/init.d/dnsmasq restart  >/dev/null 2>&1
 	rm -f /tmp/fq_auto.sh
 fi
