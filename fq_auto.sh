d9bd8518d239ccfab1a820b8918b5ecc9a950d3b
clion007 <clion007@126.com>
2017-06-16 14:46:53 +0800

    Update fq_auto.sh

--- fq_auto.sh
+++ fq_auto.sh
@@ -156,6 +156,12 @@ rm -rf /tmp/sy618
 rm -rf /tmp/racaljk.conf
 rm -rf /tmp/racaljk
 echo
+echo -e "\e[1;36m 删除被误杀的广告规则\e[0m"
+sed -i '/360/d' /tmp/fq
+sed -i '/toutiao/d' /tmp/fq
+sed -i '/taobao/d' /tmp/fq
+sed -i '/jd/d' /tmp/fq
+echo
 echo -e "\e[1;36m 删除本地规则\e[0m"
 sed -i '/::1/d' /tmp/fq
 sed -i '/localhost/d' /tmp/fq
