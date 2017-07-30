b2255da8003541fe9893829a7ccb71840463298f
clion007 <clion007@126.com>
2017-07-30 17:42:18 +0800

    Update fq_auto.sh

--- fq_auto.sh
+++ fq_auto.sh
@@ -106,9 +106,9 @@ nameserver 8.8.4.4
 nameserver 119.29.29.29
 nameserver 4.2.2.2
 nameserver 114.114.114.114
-#nameserver 1.2.4.8
-#nameserver 223.5.5.5
-#nameserver 114.114.114.119" >> /etc/dnsmasq/resolv.conf # 换成echo的方式注入
+nameserver 1.2.4.8
+nameserver 223.5.5.5
+nameserver 114.114.114.119" >> /etc/dnsmasq/resolv.conf # 换成echo的方式注入
 echo
 sleep 3
 echo -e -n "\e[1;36m 创建自定义扶墙规则\e[0m"
@@ -123,9 +123,9 @@ echo
 echo -e "\e[1;36m 下载sy618扶墙规则\e[0m"
 /usr/bin/wget-ssl --no-check-certificate -q -O /tmp/sy618 https://raw.githubusercontent.com/sy618/hosts/master/dnsmasq/dnsfq
 echo
-echo -e "\e[1;36m 下载racaljk规则\e[0m"
-/usr/bin/wget-ssl --no-check-certificate -q -O /tmp/racaljk https://raw.githubusercontent.com/racaljk/hosts/master/dnsmasq.conf
-echo
+#echo -e "\e[1;36m 下载racaljk规则\e[0m"
+#/usr/bin/wget-ssl --no-check-certificate -q -O /tmp/racaljk https://raw.githubusercontent.com/racaljk/hosts/master/dnsmasq.conf
+#echo
 sleep 3
 #echo -e "\e[1;36m 删除racaljk规则中google'youtube相关规则\e[0m"
 #sed -i '/google/d' /tmp/racaljk
@@ -136,31 +136,22 @@ cp /etc/dnsmasq.d/userlist /tmp/userlist
 echo
 echo -e -n "\e[1;36m 删除dnsmasq缓存注释\e[0m"
 sed -i '/#/d' /tmp/sy618
-sed -i '/#/d' /tmp/racaljk
+#sed -i '/#/d' /tmp/racaljk
 sed -i '/#/d' /tmp/userlist
 echo
 #echo -e -n "\e[1;36m 扶墙网站指定到#443端口访问\e[0m"
-#awk '{print $0"#443"}' /tmp/sy618 > /tmp/sy618.conf
-#awk '{print $0"#443"}' /tmp/racaljk > /tmp/racaljk.conf
-#awk '{print $0"#443"}' /tmp/userlist > /tmp/userlist.conf
+#awk '{print $0"#443"}' /tmp/sy618 > /tmp/sy618
+#awk '{print $0"#443"}' /tmp/racaljk > /tmp/racaljk
+#awk '{print $0"#443"}' /tmp/userlist > /tmp/userlist
 #echo
 echo -e -n "\e[1;36m 合并dnsmasq缓存\e[0m"
-cat /tmp/userlist /tmp/racaljk /tmp/sy618 > /tmp/fq
-#cat /tmp/userlist.conf /tmp/sy618.conf > /tmp/fq
+#cat /tmp/userlist /tmp/racaljk /tmp/sy618 > /tmp/fq
+cat /tmp/userlist /tmp/sy618 > /tmp/fq
 echo
 echo -e -n "\e[1;36m 删除dnsmasq临时文件\e[0m"
 rm -rf /tmp/userlist
-#rm -rf /tmp/userlist.conf
-#rm -rf /tmp/sy618.conf
 rm -rf /tmp/sy618
-#rm -rf /tmp/racaljk.conf
-rm -rf /tmp/racaljk
-echo
-echo -e "\e[1;36m 删除被误杀的广告规则\e[0m"
-sed -i '/360/d' /tmp/fq
-sed -i '/toutiao/d' /tmp/fq
-sed -i '/taobao/d' /tmp/fq
-sed -i '/jd/d' /tmp/fq
+#rm -rf /tmp/racaljk
 echo
 echo -e "\e[1;36m 删除本地规则\e[0m"
 sed -i '/::1/d' /tmp/fq
