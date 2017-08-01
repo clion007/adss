aea7987c07bfbbe1e256be81ff72646d396e35ce
clion007 <clion007@126.com>
2017-08-01 13:58:11 +0800

    Update fq_auto.sh

--- fq_auto.sh
+++ fq_auto.sh
@@ -4,9 +4,10 @@ wgetroute="/usr/bin/wget-ssl"
 CRON_FILE=/etc/crontabs/$USER
 clear
 echo "# Copyright (c) 2014-2017,by clion007"
+echo
 echo "# 本脚本仅用于个人研究与学习使用，从未用于产生任何盈利（包括“捐赠”等方式）"
 echo "# 未经许可，请勿内置于软件内发布与传播！请勿用于产生盈利活动！请遵守当地法律法规，文明上网。"
-echo "# openwrt类固件使用，包括但不限于pandorabox、LEDE、ddwrt等，Padavan系列固件慎用。"
+echo "# openwrt类固件使用，包括但不限于pandorabox、LEDE、ddwrt、明月、石像鬼等，华硕、老毛子、梅林等Padavan系列固件慎用。"
 echo -e "# 安装前请\e[1;31m备份原配置\e[0m；由此产生的一切后果自行承担！"
 echo -e "# 安装前请\e[1;31m检查确认路由器配置，lan IP必须是192.168.1.1\e[0m；全自动无人值守安装！"
 echo
