2edd64985b97c1bfbb66f9099ead233c103830a6
clion007 <clion007@126.com>
2017-08-01 13:56:44 +0800

    Update fqad.sh

--- fqad.sh
+++ fqad.sh
@@ -4,9 +4,10 @@ wgetroute="/usr/bin/wget-ssl"
 CRON_FILE=/etc/crontabs/$USER
 clear
 echo "# Copyright (c) 2014-2017,by clion007"
+echo
 echo "# 本脚本仅用于个人研究与学习使用，从未用于产生任何盈利（包括“捐赠”等方式）"
 echo "# 未经许可，请勿内置于软件内发布与传播！请勿用于产生盈利活动！请遵守当地法律法规，文明上网。"
-echo "# openwrt类固件使用，包括但不限于pandorabox、LEDE、ddwrt等，Padavan系列固件慎用。"
+echo "# openwrt类固件使用，包括但不限于pandorabox、LEDE、ddwrt、明月、石像鬼等，华硕、老毛子、梅林等Padavan系列固件慎用。"
 echo -e "# 安装前请\e[1;31m备份原配置\e[0m；安装过程中需要输入路由器相关配置信息，由此产生的一切后果自行承担！"
 echo
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
@@ -146,13 +147,13 @@ echo -e "\e[1;36m 下载adaway规则缓存\e[0m"
 /usr/bin/wget-ssl --no-check-certificate -q -O /tmp/adaway https://adaway.org/hosts.txt
 /usr/bin/wget-ssl --no-check-certificate -q -O /tmp/adaway2 http://winhelp2002.mvps.org/hosts.txt
 /usr/bin/wget-ssl --no-check-certificate -q -O /tmp/adaway3 http://77l5b4.com1.z0.glb.clouddn.com/hosts.txt
-#/usr/bin/wget-ssl --no-check-certificate -q -O /tmp/adaway4 https://hosts-file.net/ad_servers.txt
-/usr/bin/wget-ssl --no-check-certificate -q -O /tmp/adaway5 https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=o&mimetype=plaintext
-cat /tmp/adaway /tmp/adaway2 /tmp/adaway3 /tmp/adaway5 > /tmp/adaway.conf
+/usr/bin/wget-ssl --no-check-certificate -q -O /tmp/adaway4 https://hosts-file.net/ad_servers.txt;sed -i '/tv.sohu.com/d' /tmp/adaway4
+/usr/bin/wget-ssl --no-check-certificate -q -O /tmp/adaway5 'https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=o&mimetype=plaintext'
+cat /tmp/adaway /tmp/adaway2 /tmp/adaway3 /tmp/adaway4 /tmp/adaway5 > /tmp/adaway.conf
 rm -rf /tmp/adaway
 rm -rf /tmp/adaway2
 rm -rf /tmp/adaway3
-#rm -rf /tmp/adaway4
+rm -rf /tmp/adaway4
 rm -rf /tmp/adaway5
 echo
 sleep 3
