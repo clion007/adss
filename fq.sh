05c4a80e4fd94bb4ab8d8efa3c3fc85c476b0076
clion007 <clion007@126.com>
2017-08-09 18:45:58 +0800

    Update fq.sh

--- fq.sh
+++ fq.sh
@@ -76,8 +76,7 @@ nameserver 223.5.5.5
 nameserver 114.114.114.119" >> /etc/dnsmasq/resolv.conf
 echo
 sleep 3
-echo -e -n "\e[1;36m 创建自定义扶墙规则\e[0m"
-echo
+echo -e "\e[1;36m 创建自定义扶墙规则\e[0m"
 echo "# 格式示例如下，删除address前 # 有效，添加自定义规则
 # 正确ip地址表示DNS解析扶墙，127地址表示去广告
 #address=/.001union.com/127.0.0.1
@@ -167,6 +166,7 @@ echo -e "\e[1;36m 定时计划任务添加完成！\e[0m"
 echo
 echo -e "\e[1;36m 创建脚本更新检测副本\e[0m"
 wget --no-check-certificate -q -O /etc/dnsmasq/fq_auto.sh https://raw.githubusercontent.com/clion007/dnsmasq/master/fq_auto.sh
+echo
 sleep 1
 echo
 echo
