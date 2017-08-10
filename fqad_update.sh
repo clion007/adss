951793accf6fb3884fc5fa73e42a2d69c5b5b1b8
clion007 <clion007@126.com>
2017-08-10 10:35:14 +0800

    Update fqad_update.sh

--- fqad_update.sh
+++ fqad_update.sh
@@ -10,13 +10,17 @@ echo
 #if [ $LOGSIZE -ge 5000 ]; then
 #	sed -i -e 1,10d $LOGFILE
 #fi
-echo -e "\e[1;36m 1秒钟后开始检测更新脚本\e[0m"
+echo -e "\e[1;36m 1秒钟后开始检测更新脚本及规则\e[0m"
 echo
 wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/fqad_auto.sh -O \
       /tmp/fqad_auto.sh && chmod 775 /tmp/fqad_auto.sh
+wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/fqad_update.sh -O \
+      /tmp/fqad_update.sh && chmod 775 /tmp/fqad_update.sh
+wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/fqadrules_update.sh -O \
+      /tmp/fqadrules_update.sh && chmod 775 /tmp/fqadrules_update.sh
 if [ -s "/tmp/fqad_auto.sh" ]; then
 	if ( ! cmp -s /tmp/fqad_auto.sh /etc/dnsmasq/fqad_auto.sh ); then
-		echo " `date +'%Y-%m-%d %H:%M:%S'`: 检测到脚本更新......3秒后即将开始更新！"
+		echo " `date +'%Y-%m-%d %H:%M:%S'`: 检测到新版翻墙去广告脚本......3秒后即将开始更新！"
 		echo
 		sleep 3
 		echo -e "\e[1;36m 开始更新翻墙去广告脚本\e[0m"
@@ -24,9 +28,31 @@ if [ -s "/tmp/fqad_auto.sh" ]; then
 		clear
 		sh /tmp/fqad_auto.sh
 		echo " `date +'%Y-%m-%d %H:%M:%S'`: 翻墙去广告脚本及规则更新完成。"
+	elif ( ! cmp -s /tmp/fqad_update.sh /etc/dnsmasq/fqad_update.sh ); then
+		echo " `date +'%Y-%m-%d %H:%M:%S'`: 检测到新版升级脚本......3秒后即将开始更新！"
+		echo
+		sleep 3
+		echo -e "\e[1;36m 开始更新升级脚本\e[0m"
+		echo
+		clear
+		sh /tmp/fqad_update.sh
+		echo " `date +'%Y-%m-%d %H:%M:%S'`: 升级脚本更新完成。"
+		rm -rf /tmp/fqad_update.sh
+	elif ( ! cmp -s /tmp/fqadrules_update.sh /etc/dnsmasq/fqadrules_update.sh ); then
+		echo " `date +'%Y-%m-%d %H:%M:%S'`: 检测到新版规则升级脚本......3秒后即将开始更新！"
+		echo
+		sleep 3
+		echo -e "\e[1;36m 开始更新规则升级脚本\e[0m"
+		echo
+		clear
+		sh /tmp/fqadrules_update.sh
+		echo " `date +'%Y-%m-%d %H:%M:%S'`: 规则升级脚本更新完成。"
+		rm -rf /tmp/fqadrules_update.sh
 		else
-		echo " `date +'%Y-%m-%d %H:%M:%S'`: 脚本无需更新，3秒后即将开始更新规则"
-		rm -f /tmp/fqad_auto.sh
+		echo " `date +'%Y-%m-%d %H:%M:%S'`: 脚本已为最新，3秒后即将开始检测翻墙去广告规则更新"
+		rm -rf /tmp/fqad_auto.sh
+		rm -rf /tmp/fqad_update.sh
+		rm -rf /tmp/fqadrules_update.sh
 		sh /etc/dnsmasq/fqadrules_update.sh
 	fi	
 fi
