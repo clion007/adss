da5a995ea75f6a3a5976393a447d577f72192b96
clion007 <clion007@126.com>
2017-08-22 04:53:40 +0800

    Update fqad_auto.sh

--- fqad_auto.sh
+++ fqad_auto.sh
@@ -25,15 +25,29 @@ sleep 3
 echo -e "\e[1;36m 创建dnsmasq规则与更新脚本存放的文件夹\e[0m"
 echo
 echo -e "\e[1;36m 检测和备份当前dnsmasq配置信息\e[0m"
-if [ -d /etc/dnsmasq ]; then
-	cp -f /etc/dnsmasq /etc/dnsmasq.bak
+if [ -d /etc/dnsmasq.bak ]; then
+	filedir1=/etc/dnsmasq
+	for file1 in ${filedir1}/*
+	do
+		filename1=`basename $file1`
+		if ( ! cmp -s $filedir1/$filename1 /etc/dnsmasq.bak/$filename1 ); then
+			cp -f $filedir1/$filename1 /etc/dnsmasq.bak/$filename1
+		fi
+	done
 	else
-	mkdir -p /etc/dnsmasq
+	cp -r /etc/dnsmasq /etc/dnsmasq.bak
 fi
-if [ -d /etc/dnsmasq.d ]; then
-	cp -f /etc/dnsmasq.d /etc/dnsmasq.d.bak
+if [ -d /etc/dnsmasq.d.bak ]; then
+	filedir2=/etc/dnsmasq.d
+	for file2 in ${filedir2}/*
+	do
+		filename2=`basename $file2`
+		if ( ! cmp -s $filedir2/$filename2 /etc/dnsmasq.d.bak/$filename2 ); then
+			cp -f $filedir2/$filename2 /etc/dnsmasq.d.bak/$filename2
+		fi
+	done
 	else
-	mkdir -p /etc/dnsmasq.d
+	cp -r /etc/dnsmasq.d /etc/dnsmasq.d.bak
 fi
 if [ -f /etc/dnsmasq.conf.bak ]; then
 	echo
