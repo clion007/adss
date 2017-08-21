fdb0270a265e4b8b96e8d65e120ed8fd26aea590
clion007 <clion007@126.com>
2017-08-21 22:26:55 +0800

    Update ad.sh

--- ad.sh
+++ ad.sh
@@ -152,6 +152,7 @@ echo
 echo -e "\e[1;36m 创建广告黑名单缓存\e[0m"
 wget --no-check-certificate -q -O /tmp/adblacklist https://raw.githubusercontent.com/clion007/dnsmasq/master/adblacklist
 sort /etc/dnsmasq/userblacklist /tmp/adblacklist | uniq > /tmp/blacklist
+rm -rf /tmp/adblacklist
 sed -i "/#/d" /tmp/blacklist
 sed -i 's/^/127.0.0.1 &/g' /tmp/blacklist
 echo
