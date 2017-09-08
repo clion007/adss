dff33454285f627e23b55b33d0ef7e49b565332a
clion007 <clion007@126.com>
2017-09-08 15:03:03 +0800

    Update fq_auto.sh

--- fq_auto.sh
+++ fq_auto.sh
@@ -220,12 +220,26 @@ if [ ! $? -eq 0 ]; then
 		timedata='5'
 	fi	
 	echo "# 每天$timedata点25分更新翻墙规则
-25 $timedata * * * sh /etc/dnsmasq/fa_update.sh > /dev/null 2>&1
-# 每天6点06分重启路由器
-05 6 * * * sleep 1m && touch /etc/banner && reboot" >> $CRON_FILE
+25 $timedata * * * sh /etc/dnsmasq/fa_update.sh > /dev/null 2>&1" >> $CRON_FILE
 	/etc/init.d/cron reload
 	echo
 	echo -e "\e[1;36m 自动更新任务添加完成\e[0m"
+	echo
+fi
+sleep 1
+grep "reboot" $CRON_FILE >/dev/null
+if [ ! $? -eq 0 ]; then
+	if [ /etc/crontabs/reboottime.conf ]; then
+		reboottime=$(cat /etc/crontabs/reboottime.conf)
+		else
+		reboottime='6'
+	fi	
+	echo -e "\e[1;36m 设置路由器定时重启\e[0m"
+	echo "# 每天$reboottime点05分重启路由器
+04 $reboottime * * * sleep 1m && touch /etc/banner && reboot" >> $CRON_FILE
+	/etc/init.d/cron reload
+	echo
+	echo -e "\e[1;36m 定时重启任务设定完成\e[0m"
 	echo
 fi
 echo -e "\e[1;36m 创建脚本更新检测副本\e[0m"
