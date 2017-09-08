c3db114c1a8777f20ab5758d1f23c359f4b82569
clion007 <clion007@126.com>
2017-09-08 15:01:31 +0800

    Update fqad.sh

--- fqad.sh
+++ fqad.sh
@@ -269,7 +269,7 @@ address=/ip6-loopback/::1
 # Localhost (DO NOT REMOVE) End
 
 # Modified DNS start
-" > /etc/dnsmasq.d/fqad.conf # 换成echo的方式注入
+" > /etc/dnsmasq.d/fqad.conf
 echo
 echo -e "\e[1;36m 创建hosts规则文件\e[0m"
 echo "
@@ -289,7 +289,7 @@ echo "
 # 默认hosts结束
 
 # 修饰hosts开始
-" > /etc/dnsmasq/noad.conf # 换成echo的方式注入
+" > /etc/dnsmasq/noad.conf
 echo
 echo -e "\e[1;36m 删除dnsmasq'hosts重复规则及临时文件\e[0m"
 sort /tmp/fqad | uniq >> /etc/dnsmasq.d/fqad.conf
@@ -319,13 +319,29 @@ if [ ! $? -eq 0 ]; then
 	read timedata
 	echo "$timedata" > /etc/crontabs/Update_time.conf
 	echo "# 每天$timedata点25分更新翻墙和广告规则
-25 $timedata * * * sh /etc/dnsmasq/fqad_update.sh > /dev/null 2>&1
-# 每天6点06分重启路由器
-05 6 * * * sleep 1m && touch /etc/banner && reboot >> $CRON_FILE
+25 $timedata * * * sh /etc/dnsmasq/fqad_update.sh > /dev/null 2>&1" >> $CRON_FILE
 	/etc/init.d/cron reload
 	echo
 	echo -e "\e[1;36m 自动更新任务添加完成\e[0m"
 	echo
+fi
+sleep 1
+grep "reboot" $CRON_FILE >/dev/null
+if [ ! $? -eq 0 ]; then
+	echo -e -n "\e[1;31m 是否设置路由器定时重启（y/n）:\e[0m"
+	read rebootop
+	if [ rebootop=y ]; then
+		echo
+		echo -e -n "\e[1;36m 请输入每天定时重启时间(整点小时): \e[0m" 
+		read reboottime
+		echo "$reboottime" > /etc/crontabs/reboottime.conf
+		echo "# 每天$reboottime点05分重启路由器
+04 $reboottime * * * sleep 1m && touch /etc/banner && reboot" >> $CRON_FILE
+		/etc/init.d/cron reload
+		echo
+		echo -e "\e[1;36m 定时重启任务设定完成\e[0m"
+		echo
+	fi	
 fi
 echo -e "\e[1;36m 创建脚本更新检测副本\e[0m"
 wget --no-check-certificate -q -O /etc/dnsmasq/fqad_auto.sh https://raw.githubusercontent.com/clion007/dnsmasq/master/fqad_auto.sh
