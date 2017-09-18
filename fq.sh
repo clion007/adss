051a1cbbf65dc0e1ca02422fbb5ef0629c9b7302
clion007 <clion007@126.com>
2017-09-18 11:42:40 +0800

    Update fq.sh

--- fq.sh
+++ fq.sh
@@ -215,7 +215,7 @@ if [ ! $? -eq 0 ]; then
 	read timedata
 	echo "$timedata" > /etc/crontabs/Update_time.conf
 	echo "# 每天$timedata点25分更新翻墙规则
-25 $timedata * * * sh /etc/dnsmasq/fa_update.sh > /dev/null 2>&" >> $CRON_FILE
+25 $timedata * * * sh /etc/dnsmasq/fq_update.sh > /dev/null 2>&" >> $CRON_FILE
 	/etc/init.d/cron reload
 	echo
 	echo -e "\e[1;36m 自动更新任务添加完成\e[0m"
