8b0b9a0e06edb2cf5c7a32bb935232b45ef4a712
clion007 <clion007@126.com>
2020-05-03 15:12:34 +0000

    Update ad.sh

--- ad.sh
+++ ad.sh
@@ -248,7 +248,7 @@ echo
 echo -e "\e[1;36m 创建dnsmasq规则文件\e[0m"
 echo "
 ############################################################
-## 【Copyright (c) 2014-2017, clion007】                          ##
+## 【Copyright (c) 2014-2020, clion007】                          ##
 ##                                                                ##
 ## 感谢https://github.com/vokins/hosts                            ##
 ##                                                                ##
@@ -266,7 +266,7 @@ echo
 echo -e "\e[1;36m 创建hosts规则文件\e[0m"
 echo "
 ############################################################
-## 【Copyright (c) 2014-2017, clion007】                          ##
+## 【Copyright (c) 2014-2020, clion007】                          ##
 ##                                                                ##
 ## 感谢https://github.com/vokins/hosts                            ##
 ##                                                                ##
