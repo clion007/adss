c47e54a162487005f191d227b5db5c5564cd937e
clion007 <clion007@126.com>
2020-05-03 15:17:58 +0000

    Update adrules_update.sh

--- adrules_update.sh
+++ adrules_update.sh
@@ -6,8 +6,17 @@ echo
 echo -e "\e[1;36m 下载vokins广告规则\e[0m"
 wget --no-check-certificate -q -O /tmp/ad.conf https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/union.conf
 echo
-echo -e "\e[1;36m 下载easylistchina广告规则\e[0m"
-wget --no-check-certificate -q -O /tmp/easylistchina.conf https://c.nnjsx.cn/GL/dnsmasq/update/adblock/easylistchina.txt
+echo -e "\e[1;36m 下载yoyoAd广告规则\e[0m"
+wget --no-check-certificate -q -O /tmp/yoyoAd.conf https://pgl.yoyo.org/adservers/serverlist.php?hostformat=dnsmasq;showintro=0
+echo
+echo -e "\e[1;36m 下载notrackAd广告规则\e[0m"
+wget --no-check-certificate -q -O /tmp/notrackAdDomain.conf https://raw.githubusercontent.com/notracking/hosts-blocklists/master/domains.txt
+wget --no-check-certificate -q -O /tmp/notrackAdhosts.conf https://raw.githubusercontent.com/notracking/hosts-blocklists/master/hostnames.txt
+echo
+echo -e "\e[1;36m 下载Anti-Ad广告规则\e[0m"
+wget --no-check-certificate -q -O /tmp/antiAd.conf https://gitee.com/privacy-protection-tools/anti-ad/raw/master/anti-ad-for-dnsmasq.conf
+sed -i "/#/d" /tmp/antiAd.conf
+sed -i 's/$/&127.0.0.1/g' /tmp/antiAd.conf
 echo
 echo -e "\e[1;36m 下载yhosts缓存\e[0m"
 wget --no-check-certificate -q -O /tmp/yhosts.conf https://raw.githubusercontent.com/vokins/yhosts/master/hosts.txt
@@ -18,10 +27,8 @@ echo
 echo -e "\e[1;36m 下载adaway规则缓存\e[0m"
 wget --no-check-certificate -q -O /tmp/adaway https://adaway.org/hosts.txt
 wget --no-check-certificate -q -O /tmp/adaway2 http://winhelp2002.mvps.org/hosts.txt && sed -i "s/.$//g" /tmp/adaway2
-wget --no-check-certificate -q -O /tmp/adaway4 https://hosts-file.net/ad_servers.txt && sed -i "s/.$//g" /tmp/adaway4
-#wget --no-check-certificate -q -O /tmp/adaway5 https://pgl.yoyo.org/adservers/serverlist.php?showintro=0;hostformat=hosts
-cat /tmp/adaway /tmp/adaway2 /tmp/adaway3 /tmp/adaway4 > /tmp/adaway.conf
-rm -rf /tmp/adaway /tmp/adaway2 /tmp/adaway3 /tmp/adaway4 #/tmp/adaway5
+cat /tmp/adaway /tmp/adaway2 > /tmp/adaway.conf
+rm -rf /tmp/adaway /tmp/adaway2
 echo
 sleep 3
 echo -e "\e[1;36m 创建用户自定规则缓存\e[0m"
@@ -36,11 +43,11 @@ sed -i "/#/d" /tmp/blacklist
 sed -i '/./{s|^|address=/|;s|$|/127.0.0.1|}' /tmp/blacklist #改为dnsmasq方式，支持通配符
 echo
 echo -e "\e[1;36m 合并dnsmasq、hosts缓存\e[0m"
-cat /tmp/userlist /tmp/ad.conf /tmp/easylistchina.conf /tmp/blacklist > /tmp/ad
-cat /tmp/yhosts.conf /tmp/adaway.conf /tmp/mallist > /tmp/noad
+cat /tmp/userlist /tmp/ad.conf /tmp/yoyoAd.conf /tmp/notrackAdDomain.conf /tmp/antiAd.conf /tmp/blacklist > /tmp/ad
+cat /tmp/notrackAdhosts.conf /tmp/yhosts.conf /tmp/adaway.conf /tmp/mallist > /tmp/noad
 echo
 echo -e "\e[1;36m 删除dnsmasq、hosts临时文件\e[0m"
-rm -rf /tmp/userlist /tmp/ad.conf /tmp/easylistchina.conf /tmp/blacklist /tmp/yhosts.conf /tmp/adaway.conf /tmp/mallist
+rm -rf /tmp/userlist /tmp/ad.conf /tmp/yoyoAd.conf /tmp/notrackAdDomain.conf /tmp/antiAd.conf /tmp/blacklist /tmp/notrackAdhosts.conf /tmp/yhosts.conf /tmp/adaway.conf /tmp/mallist
 echo
 echo -e "\e[1;36m 删除被误杀的广告规则\e[0m"
 wget --no-check-certificate -q -O /tmp/adwhitelist https://raw.githubusercontent.com/clion007/dnsmasq/master/adwhitelist
@@ -73,7 +80,7 @@ echo
 echo -e "\e[1;36m 创建dnsmasq规则文件\e[0m"
 echo "
 ############################################################
-## 【Copyright (c) 2014-2017, clion007】                          ##
+## 【Copyright (c) 2014-2020, clion007】                          ##
 ##                                                                ##
 ## 感谢https://github.com/vokins/hosts                            ##
 ##                                                                ##
@@ -91,7 +98,7 @@ echo
 echo -e "\e[1;36m 创建hosts规则文件\e[0m"
 echo "
 ############################################################
-## 【Copyright (c) 2014-2017, clion007】                          ##
+## 【Copyright (c) 2014-2020 clion007】                          ##
 ##                                                                ##
 ## 感谢https://github.com/vokins/hosts                            ##
 ##                                                                ##
