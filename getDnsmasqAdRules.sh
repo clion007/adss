0eefa4be06b17e01e784f935b5ea86d46bebce2c
clion007 <clion007@126.com>
2020-05-04 16:08:16 +0000

    Update getDnsmasqAdRules.sh

--- getDnsmasqAdRules.sh
+++ getDnsmasqAdRules.sh
@@ -2,22 +2,22 @@
 echo -e "\e[1;36m 开始下载Dnsmasq广告规则\e[0m"
 echo
 echo -e "\e[1;36m 下载vokins广告规则\e[0m"
-wget --no-check-certificate -c -O /tmp/vokins.conf https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/union.conf
+wget --no-check-certificate -c -t 5 -T 60 -O /tmp/vokins.conf https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/union.conf
 echo
 echo -e "\e[1;36m 下载yoyoAd广告规则\e[0m"
-wget --no-check-certificate -c -O /tmp/yoyoAd.conf https://pgl.yoyo.org/adservers/serverlist.php?hostformat=dnsmasq\&showintro=0\&mimetype=plaintext
+wget --no-check-certificate -c -t 5 -T 60 -O /tmp/yoyoAd.conf https://pgl.yoyo.org/adservers/serverlist.php?hostformat=dnsmasq\&showintro=0\&mimetype=plaintext
 echo
 echo -e "\e[1;36m 下载notrackAd广告规则\e[0m"
-wget --no-check-certificate -c -O /tmp/notrackAdDomain.conf https://raw.githubusercontent.com/notracking/hosts-blocklists/master/domains.txt
+wget --no-check-certificate -c -t 5 -T 60 -O /tmp/notrackAdDomain.conf https://raw.githubusercontent.com/notracking/hosts-blocklists/master/domains.txt
 echo
 echo -e "\e[1;36m 下载Anti-Ad广告规则\e[0m"
-wget --no-check-certificate -c -O /tmp/antiAd.conf https://gitee.com/privacy-protection-tools/anti-ad/raw/master/anti-ad-for-dnsmasq.conf
+wget --no-check-certificate -c -t 5 -T 60 -O /tmp/antiAd.conf https://gitee.com/privacy-protection-tools/anti-ad/raw/master/anti-ad-for-dnsmasq.conf
 sed -i "/#/d" /tmp/antiAd.conf
 sed -i 's/$/&127.0.0.1/g' /tmp/antiAd.conf
 echo
 sleep 3
 echo -e "\e[1;36m 创建广告黑名单缓存\e[0m"
-wget --no-check-certificate -c -O /tmp/adblacklist https://raw.githubusercontent.com/clion007/dnsmasq/master/adblacklist
+wget --no-check-certificate -c -t 5 -T 60 -O /tmp/adblacklist https://raw.githubusercontent.com/clion007/dnsmasq/master/adblacklist
 sort /etc/dnsmasq/userblacklist /tmp/adblacklist | uniq > /tmp/blacklist
 rm -rf /tmp/adblacklist
 sed -i "/#/d" /tmp/blacklist
