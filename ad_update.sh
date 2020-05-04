cc0e9f2af78fa2929a3076fc1e83b0b73985a5de
clion007 <clion007@126.com>
2020-05-04 08:03:59 +0000

    Update ad_update.sh

--- ad_update.sh
+++ ad_update.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-echo
-echo " Copyright (c) 2014-2020,by clion007"
-echo
-echo " 本脚本仅用于个人研究与学习使用，从未用于产生任何盈利（包括“捐赠”等方式）"
-echo " 未经许可，请勿内置于软件内发布与传播！请勿用于产生盈利活动！请遵守当地法律法规，文明上网。"
+if [ ! -s /tmp/copyright.sh ]; then
+	wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/copyright.sh -O \
+		/tmp/copyright.sh && chmod 775 /tmp/copyright.sh && sh /tmp/copyright.sh
+	else
+		sh /tmp/copyright.sh
+fi
 echo
 echo -e "\e[1;36m 1秒钟后开始检测更新脚本及规则\e[0m"
 echo
@@ -12,7 +13,20 @@ wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/m
 wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/adrules_update.sh -O \
       /tmp/adrules_update.sh && chmod 775 /tmp/adrules_update.sh
 if [ -s "/tmp/ad_update.sh" ]; then
-	if ( ! cmp -s /tmp/ad_update.sh /etc/dnsmasq/ad_update.sh ); then
+	if  ( ! cmp -s /tmp/ad_auto.sh /etc/dnsmasq/ad_auto.sh ); then
+		echo " `date +'%Y-%m-%d %H:%M:%S'`: 检测到新版翻墙脚本......3秒后即将开始更新！"
+		echo
+		sleep 3
+		echo -e "\e[1;36m 开始更新翻墙脚本\e[0m"
+		echo
+		wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/uninstall.sh -O \
+			/tmp/ad_auto.sh && chmod 775 /tmp/ad_auto.sh && sh /tmp/uninstall.sh > /dev/null 2>&1
+		rm -f /tmp/uninstall.sh
+		sh /tmp/ad_auto.sh
+		rm -rf /tmp/ad_update.sh /tmp/adrules_update.sh
+		echo " `date +'%Y-%m-%d %H:%M:%S'`: 翻墙脚本及规则更新完成。"
+		exit 0
+		elif( ! cmp -s /tmp/ad_update.sh /etc/dnsmasq/ad_update.sh ); then
 			echo " `date +'%Y-%m-%d %H:%M:%S'`: 检测到新版升级脚本......3秒后即将开始更新！"
 			echo
 			sleep 3
@@ -20,8 +34,9 @@ if [ -s "/tmp/ad_update.sh" ]; then
 			echo
 			sh /tmp/ad_update.sh
 			mv -f /tmp/ad_update.sh /etc/dnsmasq/ad_update.sh
-			rm -rf /tmp/adrules_update.sh
+			rm -rf /tmp/ad_auto.sh /tmp/adrules_update.sh
 			echo " `date +'%Y-%m-%d %H:%M:%S'`: 升级脚本更新完成。"
+			exit 0
 		elif ( ! cmp -s /tmp/adrules_update.sh /etc/dnsmasq/adrules_update.sh ); then
 			echo " `date +'%Y-%m-%d %H:%M:%S'`: 检测到新版规则升级脚本......3秒后即将开始更新！"
 			echo
@@ -30,17 +45,19 @@ if [ -s "/tmp/ad_update.sh" ]; then
 			echo
 			sh /tmp/adrules_update.sh
 			mv -f /tmp/adrules_update.sh /etc/dnsmasq/adrules_update.sh
-			rm -rf /tmp/ad_update.sh
+			rm -rf /tmp/ad_auto.sh /tmp/ad_update.sh
 			echo " `date +'%Y-%m-%d %H:%M:%S'`: 规则升级脚本更新完成。"
+			exit 0
 		else
-		echo " `date +'%Y-%m-%d %H:%M:%S'`: 脚本已为最新，3秒后即将开始检测去广告规则更新"
+		echo " `date +'%Y-%m-%d %H:%M:%S'`: 脚本已为最新，3秒后即将开始检测规则更新"
 		sh /etc/dnsmasq/adrules_update.sh
 		rm -rf /tmp/ad_update.sh /tmp/adrules_update.sh
 		echo " `date +'%Y-%m-%d %H:%M:%S'`: 规则已经更新完成。"
+		exit 0
 	fi	
 	else
-	echo -e "\e[1;36m  `date +'%Y-%m-%d %H:%M:%S'`: 网络异常检查脚本更新失败，稍后尝试规则更新。\e[0m"
-	sh /etc/dnsmasq/fqrules_update.sh
+	echo -e "\e[1;36m  `date +'%Y-%m-%d %H:%M:%S'`: 网络连接异常，稍后尝试进行规则更新。\e[0m"
+	sh /etc/dnsmasq/adrules_update.sh
 fi
-echo
+rm -f  /tmp/copyright.sh
 exit 0
