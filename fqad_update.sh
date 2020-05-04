809e1f31ee3ff6ecb9b6f3ab400ba038954d32d0
clion007 <clion007@126.com>
2020-05-04 10:28:39 +0000

    Update fqad_update.sh

--- fqad_update.sh
+++ fqad_update.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
+clear
 echo
-echo " Copyright (c) 2014-2017,by clion007"
-echo
-echo " 本脚本仅用于个人研究与学习使用，从未用于产生任何盈利（包括“捐赠”等方式）"
-echo " 未经许可，请勿内置于软件内发布与传播！请勿用于产生盈利活动！请遵守当地法律法规，文明上网。"
-echo
-#LOGFILE=/tmp/fqad_update.log
-#LOGSIZE=$(wc -c < $LOGFILE)
-#if [ $LOGSIZE -ge 5000 ]; then
-#	sed -i -e 1,10d $LOGFILE
-#fi
+if [ ! -s /tmp/copyright.sh ]; then
+	wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/copyright.sh -O \
+		/tmp/copyright.sh && chmod 775 /tmp/copyright.sh && sh /tmp/copyright.sh
+	else
+		sh /tmp/copyright.sh
+fi
+rm -f /tmp/copyright.sh
 echo -e "\e[1;36m 1秒钟后开始检测更新脚本及规则\e[0m"
 echo
+sleep 1
 wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/fqad_auto.sh -O \
       /tmp/fqad_auto.sh && chmod 775 /tmp/fqad_auto.sh
 wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/fqad_update.sh -O \
@@ -25,9 +24,13 @@ if [ -s "/tmp/fqad_auto.sh" ]; then
 		sleep 3
 		echo -e "\e[1;36m 开始更新翻墙去广告脚本\e[0m"
 		echo
+		wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/uninstall.sh -O \
+			/tmp/ad_auto.sh && chmod 775 /tmp/ad_auto.sh && sh /tmp/uninstall.sh > /dev/null 2>&1
+		rm -f /tmp/uninstall.sh
 		sh /tmp/fqad_auto.sh
 		rm -rf /tmp/fqad_update.sh /tmp/fqadrules_update.sh
 		echo " `date +'%Y-%m-%d %H:%M:%S'`: 翻墙去广告脚本及规则更新完成。"
+		exit 0
 	elif ( ! cmp -s /tmp/fqad_update.sh /etc/dnsmasq/fqad_update.sh ); then
 		echo " `date +'%Y-%m-%d %H:%M:%S'`: 检测到新版升级脚本......3秒后即将开始更新！"
 		echo
@@ -38,6 +41,7 @@ if [ -s "/tmp/fqad_auto.sh" ]; then
 		mv -f /tmp/fqad_update.sh /etc/dnsmasq/fqad_update.sh
 		rm -rf /tmp/fqad_auto.sh /tmp/fqadrules_update.sh
 		echo " `date +'%Y-%m-%d %H:%M:%S'`: 升级脚本更新完成。"
+		exit 0
 	elif ( ! cmp -s /tmp/fqadrules_update.sh /etc/dnsmasq/fqadrules_update.sh ); then
 		echo " `date +'%Y-%m-%d %H:%M:%S'`: 检测到新版规则升级脚本......3秒后即将开始更新！"
 		echo
@@ -48,15 +52,25 @@ if [ -s "/tmp/fqad_auto.sh" ]; then
 		mv -f /tmp/fqadrules_update.sh /etc/dnsmasq/fqadrules_update.sh
 		rm -rf /tmp/fqad_auto.sh /tmp/fqad_update.sh
 		echo " `date +'%Y-%m-%d %H:%M:%S'`: 规则升级脚本更新完成。"
+		exit 0
 		else
 		echo " `date +'%Y-%m-%d %H:%M:%S'`: 脚本已为最新，3秒后即将开始检测翻墙去广告规则更新"
 		sh /etc/dnsmasq/fqadrules_update.sh
 		rm -rf /tmp/fqad_auto.sh /tmp/fqad_update.sh /tmp/fqadrules_update.sh
 		echo " `date +'%Y-%m-%d %H:%M:%S'`: 规则已经更新完成。"
+		exit 0
 	fi	
 	else
-	echo -e "\e[1;36m  `date +'%Y-%m-%d %H:%M:%S'`: 网络异常检查脚本更新失败，稍后尝试规则更新。\e[0m"
+	if ( -f /tmp/fqad_auto.sh); then
+		rm -f  /tmp/fqad_auto.sh
+	fi	
+	if ( -f /tmp/fqad_update.sh); then
+		rm -f  /tmp/fqad_update.sh
+	fi	
+	if ( -f /tmp/fqadrules_update.sh); then
+		rm -f  /tmp/fqadrules_update.sh
+	fi	
+	echo -e "\e[1;36m  `date +'%Y-%m-%d %H:%M:%S'`: 网络连接异常，稍后尝试规则更新。\e[0m"
 	sh /etc/dnsmasq/fqadrules_update.sh
 fi
-echo
 exit 0
