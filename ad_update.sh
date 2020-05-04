059e57ce9c13407cbb0b439d045d10d3999331b4
clion007 <clion007@126.com>
2020-05-04 15:30:00 +0000

    Update ad_update.sh

--- ad_update.sh
+++ ad_update.sh
@@ -26,7 +26,7 @@ if [ -s "/tmp/ad_update.sh" ]; then
 		rm -f /tmp/uninstall.sh
 		sh /tmp/ad_auto.sh
 		rm -rf /tmp/ad_update.sh /tmp/adrules_update.sh
-		echo " `date +'%Y-%m-%d %H:%M:%S'`: 翻墙脚本及规则更新完成。"
+		echo " `date +'%Y-%m-%d %H:%M:%S'`: 脚本及规则更新完成。"
 		echo
 	elif( ! cmp -s /tmp/ad_update.sh /etc/dnsmasq/ad_update.sh ); then
 		echo " `date +'%Y-%m-%d %H:%M:%S'`: 检测到新版升级脚本......3秒后即将开始更新！"
@@ -34,8 +34,8 @@ if [ -s "/tmp/ad_update.sh" ]; then
 		sleep 3
 		echo -e "\e[1;36m 开始更新升级脚本\e[0m"
 		echo
-		sh /tmp/ad_update.sh
 		mv -f /tmp/ad_update.sh /etc/dnsmasq/ad_update.sh
+		sh /etc/dnsmasq/ad_update.sh
 		rm -rf /tmp/ad_auto.sh /tmp/adrules_update.sh
 		echo " `date +'%Y-%m-%d %H:%M:%S'`: 升级脚本更新完成。"
 		echo
@@ -45,8 +45,8 @@ if [ -s "/tmp/ad_update.sh" ]; then
 		sleep 3
 		echo -e "\e[1;36m 开始更新规则升级脚本\e[0m"
 		echo
-		sh /tmp/adrules_update.sh
 		mv -f /tmp/adrules_update.sh /etc/dnsmasq/adrules_update.sh
+		sh /etc/dnsmasq/adrules_update.sh
 		rm -rf /tmp/ad_auto.sh /tmp/ad_update.sh
 		echo " `date +'%Y-%m-%d %H:%M:%S'`: 规则升级脚本更新完成。"
 		echo
