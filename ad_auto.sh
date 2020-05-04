1a4588a868104cd165c6934c93b057b3c9a62e5f
clion007 <clion007@126.com>
2020-05-04 10:10:27 +0000

    Update ad_auto.sh

--- ad_auto.sh
+++ ad_auto.sh
@@ -12,32 +12,34 @@ echo
 echo -e "\e[1;36m 三秒后开始备份安装前路由器相关配置......\e[0m"
 echo
 sleep 3
-	wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/backup.sh -O \
-		/tmp/backup.sh && chmod 775 /tmp/backup.sh.sh && sh /tmp/backup.sh
-	rm -f /tmp/backup.sh
+wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/backup.sh -O \
+	/tmp/backup.sh && chmod 775 /tmp/backup.sh.sh && sh /tmp/backup.sh
+rm -f /tmp/backup.sh
 echo
 sleep 3
 wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/modifyConfig.sh -c -O \
 	/tmp/modifyConfig.sh  && chmod 775 /tmp/modifyConfig.sh  && sh /tmp/modifyConfig.sh
-	rm -f /tmp/modifyConfig.sh
+rm -f /tmp/modifyConfig.sh
 echo
-wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/creatRulesFile.sh -c -O \
-	/tmp/creatRulesFile.sh  && chmod 775 /tmp/creatRulesFile.sh  && sh /tmp/creatRulesFile.sh
-	rm -f /tmp/creatRulesFile.sh
+wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/initRulesFile.sh -c -O \
+	/tmp/initRulesFile.sh  && chmod 775 /tmp/initRulesFile.sh  && sh /tmp/initRulesFile.sh
+rm -f /tmp/initRulesFile.sh
 echo
 wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/getDnsmasqAdRules.sh -c -O \
 	/tmp/getDnsmasqAdRules.sh  && chmod 775 /tmp/getDnsmasqAdRules.sh  && sh /tmp/getDnsmasqAdRules.sh
-	rm -f /tmp/getDnsmasqAdRules.sh
+rm -f /tmp/getDnsmasqAdRules.sh
 echo
 wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/getHostsAdRules.sh -c -O \
 	/tmp/getHostsAdRules.sh  && chmod 775 /tmp/getHostsAdRules.sh  && sh /tmp/getHostsAdRules.sh
-	rm -f /tmp/getHostsAdRules.sh
+rm -f /tmp/getHostsAdRules.sh
 echo
 wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/deletWhiteListRules.sh -c -O \
 	/tmp/deletWhiteListRules.sh  && chmod 775 /tmp/deletWhiteListRules.sh  && sh /tmp/deletWhiteListRules.sh
-	rm -f /tmp/deletWhiteListRules.sh
+rm -f /tmp/deletWhiteListRules.sh
 echo
 echo -e "\e[1;36m 删除dnsmasq,hosts重复规则及临时文件\e[0m"
+mv /tmp/dnsrules.conf /etc/dnsmasq.d/dnsrules.conf
+mv /tmp/hostsrules.conf /etc/dnsmasq.d/hostsrules.conf
 sort /tmp/dnsAd | uniq >> /etc/dnsmasq.d/dnsrules.conf
 sort /tmp/hostsAd | uniq >> /etc/dnsmasq/hostsrules.conf
 rm -rf /tmp/dnsAd /tmp/hostsAd
@@ -58,10 +60,10 @@ echo
 echo -e "\e[1;36m 获取规则更新脚本\e[0m"
 wget --no-check-certificate -c -O /etc/dnsmasq/adrules_update.sh https://raw.githubusercontent.com/clion007/dnsmasq/master/adrules_update.sh && chmod 755 /etc/dnsmasq/adrules_update.sh
 echo
-sleep 1
-	wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/cron.sh -O \
-		/tmp/cron.sh.sh && chmod 775 /tmp/cron.sh.sh && sh /tmp/cron.sh.sh
-	rm -f /tmp/cron.sh
+sleep 3
+wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/cron.sh -O \
+	/tmp/cron.sh.sh && chmod 775 /tmp/cron.sh.sh && sh /tmp/cron.sh.sh
+rm -f /tmp/cron.sh
 echo -e "\e[1;36m 创建脚本更新检测副本\e[0m"
 if [ -s /tmp/ad_auto.sh ]; then
 	mv -f /tmp/ad_auto.sh /etc/dnsmasq/ad_auto.sh
