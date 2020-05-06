650aeb215af2b36646f0756e1bf52685dda74802
clion007 <clion007@126.com>
2020-05-06 07:12:23 +0000

    Update fqad_auto.sh

--- fqad_auto.sh
+++ fqad_auto.sh
@@ -1,8 +1,30 @@
 #!/bin/sh
+if [ -s "/etc/dnsmasq/resolv.conf" ]; then
+	rm -f /var/lock/opkg.lock
+	rm -rf /etc/dnsmasq
+	rm -rf /etc/dnsmasq.d
+	if [ -d /etc/dnsmasq.bak ]; then
+		if [ -s /etc/dnsmasq/originalState ]; then
+			mv -f /etc/dnsmasq.bak /etc/dnsmasq
+		else
+			rm -rf /etc/dnsmasq.bak
+		fi
+	fi	
+	if [ -d /etc/dnsmasq.d.bak ]; then
+		if [ -s /etc/dnsmasq.d/originalState ]; then
+			mv -f /etc/dnsmasq.d.bak /etc/dnsmasq.d
+		else
+			rm -rf /etc/dnsmasq.d.bak
+		fi
+	fi
+	if [ -f /etc/dnsmasq.conf.bak ]; then
+		mv -f /etc/dnsmasq.conf.bak /etc/dnsmasq.conf
+	fi	
+fi
 clear
 echo
 if [ ! -s /tmp/copyright.sh ]; then
-	wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/copyright.sh -O \
+	wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/copyright.sh -qO \
 		/tmp/copyright.sh && chmod 775 /tmp/copyright.sh && sh /tmp/copyright.sh
 	else
 		sh /tmp/copyright.sh
@@ -12,16 +34,16 @@ echo
 echo -e "\e[1;36m 三秒后开始备份安装前路由器相关配置......\e[0m"
 echo
 sleep 3
-	wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/backup.sh -O \
+	wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/backup.sh -qO \
 		/tmp/backup.sh && chmod 775 /tmp/backup.sh && sh /tmp/backup.sh
 rm -f /tmp/backup.sh
 echo
 sleep 3
-wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/modifyConfig.sh -c -O \
+wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/modifyConfig.sh -c -q -O \
 	/tmp/modifyConfig.sh  && chmod 775 /tmp/modifyConfig.sh  && sh /tmp/modifyConfig.sh
 rm -f /tmp/modifyConfig.sh
 echo
-wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/initRulesFile.sh -c -O \
+wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/initRulesFile.sh -c -q -O \
 	/tmp/initRulesFile.sh  && chmod 775 /tmp/initRulesFile.sh  && sh /tmp/initRulesFile.sh
 rm -f /tmp/initRulesFile.sh
 echo
@@ -33,15 +55,15 @@ sed -i '/::1/d' /tmp/googlehosts
 sed -i '/localhost/d' /tmp/googlehosts
 sed -i '/#/d' /tmp/googlehosts
 echo
-wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/getDnsmasqAdRules.sh -c -O \
+wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/getDnsmasqAdRules.sh -c -q -O \
 	/tmp/getDnsmasqAdRules.sh  && chmod 775 /tmp/getDnsmasqAdRules.sh  && sh /tmp/getDnsmasqAdRules.sh
 rm -f /tmp/getDnsmasqAdRules.sh
 echo
-wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/getHostsAdRules.sh -c -O \
+wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/getHostsAdRules.sh -c -q -O \
 	/tmp/getHostsAdRules.sh  && chmod 775 /tmp/getHostsAdRules.sh  && sh /tmp/getHostsAdRules.sh
 rm -f /tmp/getHostsAdRules.sh
 echo
-wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/deletWhiteListRules.sh -c -O \
+wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/deletWhiteListRules.sh -c -q -O \
 	/tmp/deletWhiteListRules.sh  && chmod 775 /tmp/deletWhiteListRules.sh  && sh /tmp/deletWhiteListRules.sh
 rm -f /tmp/deletWhiteListRules.sh
 echo
@@ -72,7 +94,7 @@ echo -e "\e[1;36m 获取规则更新脚本\e[0m"
 wget --no-check-certificate -q -O /etc/dnsmasq/fqadrules_update.sh https://raw.githubusercontent.com/clion007/dnsmasq/master/fqadrules_update.sh && chmod 755 /etc/dnsmasq/fqadrules_update.sh
 echo
 sleep 3
-wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/cron.sh -O \
+wget --no-check-certificate https://raw.githubusercontent.com/clion007/dnsmasq/master/cron.sh -qO \
 	/tmp/cron.sh && chmod 775 /tmp/cron.sh && sh /tmp/cron.sh
 rm -f /tmp/cron.sh
 echo -e "\e[1;36m 创建脚本更新检测副本\e[0m"
