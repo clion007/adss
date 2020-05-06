6651949726a1b4146abebecadc4385e3f3fd51ba
clion007 <clion007@126.com>
2020-05-06 06:49:38 +0000

    Update modifyConfig.sh

--- modifyConfig.sh
+++ modifyConfig.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+scriptfile=/tmp/*ad*.sh
 sleep 3
 grep "hostsrules" /etc/dnsmasq.conf >/dev/null
 if [ ! $? -eq 0 ]; then
@@ -69,13 +70,11 @@ if [ ! -f /etc/dnsmasq.d/userlist ]; then
 	echo
 	echo -e "\e[1;36m 开始创建创建自定义dnsmasq规则\e[0m"
 	echo "# 格式示例如下，删除address前 # 有效，添加自定义规则
-# 正确ip地址表示DNS解析扶墙，127地址表示去广告
-#address=/.001union.com/127.0.0.1
+# 后面的ip表示希望域名解析的正确IP
 #address=/telegram.org/149.154.167.99" > /etc/dnsmasq.d/userlist
 	echo
 fi
-scriptfile=/tmp/*ad*.sh
-if [ -f $scriptfile ]; then
+if [ -f "$scriptfile" ]; then
 	if [ ! -f /etc/dnsmasq/userblacklist ]; then
 		echo -e "\e[1;36m 创建自定义广告黑名单\e[0m"
 		echo
