1dc769edacca3cd54340049b384944ec6e1ccdd8
clion007 <clion007@126.com>
2020-05-24 04:37:06 +0000

    Update deletWhiteListRules.sh

--- deletWhiteListRules.sh
+++ deletWhiteListRules.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo -e "\e[1;36m 删除白名单及误杀规则\e[0m"
+echo -e "\e[1;36m 删除白名单及误杀规则，时间较长，请耐心等待。。。\e[0m"
 wget --no-check-certificate -c -q -O /tmp/adwhitelist https://raw.githubusercontent.com/clion007/dnsmasq/master/adwhitelist
 sort /etc/dnsmasq/userwhitelist /tmp/adwhitelist | uniq > /tmp/whitelist
 sed -i "/#/d" /tmp/whitelist
