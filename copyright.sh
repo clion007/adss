5fc661b35087ca2a2f50688e80bc8bb3e4b98626
clion007 <clion007@126.com>
2023-10-20 16:43:19 +0800

    Update copyright.sh

--- copyright.sh
+++ copyright.sh
@@ -4,7 +4,7 @@ echo "Auto DNS Science Internet V3.1.0"
 echo "Copyright @ 2014-`date +'%Y'`,by clion007"
 echo
 echo "本脚本仅用于个人研究与学习使用，从未用于产生任何盈利（包括“捐赠”等方式）"
-echo "未经许可，请勿内置于软件内发布与传播！请勿用于产生盈利活动！请遵守当地法律法规，文明上网。"
+echo "未经许可，请勿内置于软件内发布与传播！请勿用于产生盈利活动！请遵守当地法律法规，文明上网!"
 echo "openwrt类固件使用，包括但不限于pandorabox、LEDE、ddwrt、明月、石像鬼等，华硕、老毛子、梅林等Padavan系列固件慎用。"
 echo -e "安装前请\e[1;31m备份原配置\e[0m；由此产生的一切后果自行承担！"
 echo -e "支持全自动无人值守安装和手动配置安装！"
