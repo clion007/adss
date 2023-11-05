61ed35920570c2380ce4d4695b5cb152c88a1e98
clion007 <clion007@example.com>
2023-11-05 21:53:41 +0800

    Update adrules_update.sh

--- adrules_update.sh
+++ adrules_update.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 sleep 3
 wget --no-check-certificate -c -q -O /tmp/initRulesFile.sh https://gitcode.net/clion007/dnsmasq/raw/master/initRulesFile.sh
 chmod 775 /tmp/initRulesFile.sh
