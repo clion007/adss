81a31e40493db01c2a29fe87068f4cea2cd0c4f4
clion007 <clion007@example.com>
2023-11-05 10:40:04 +0800

    Update getHostsAdRules.sh

--- getHostsAdRules.sh
+++ getHostsAdRules.sh
@@ -4,26 +4,14 @@ echo
 echo -e "\e[1;36m 下载someonewhocares缓存\e[0m"
 wget --no-check-certificate -c -q -T 60 -O /tmp/someonewhocares.conf https://someonewhocares.org/hosts/zero/hosts
 echo
-echo -e "\e[1;36m 下载malwaredomainlist规则\e[0m"
-wget --no-check-certificate -c -q -T 60 -O /tmp/mallist http://www.malwaredomainlist.com/hostslist/hosts.txt
-sed -i "s/.$//g" /tmp/mallist
-echo
 echo -e "\e[1;36m 下载adaway规则缓存\e[0m"
-wget --no-check-certificate -c -q -T 60 -O /tmp/adaway https://adaway.org/hosts.txt
-wget --no-check-certificate -c -q -T 60 -O /tmp/adaway2 http://winhelp2002.mvps.org/hosts.txt
-sed -i "s/.$//g" /tmp/adaway2
-echo
-echo -e "\e[1;36m adaway规则下载完成，开始合并规则\e[0m"
-cat /tmp/adaway /tmp/adaway2 > /tmp/adaway.conf
-echo
-echo -e "\e[1;36m adaway规则合并完成，清除生成的规则缓存文件\e[0m"
-rm -rf /tmp/adaway /tmp/adaway2
+wget --no-check-certificate -c -q -T 60 -O /tmp/adaway.conf https://adaway.org/hosts.txt
 echo
 echo -e "\e[1;36m 合并hosts缓存\e[0m"
-cat /tmp/someonewhocares.conf /tmp/adaway.conf /tmp/mallist > /tmp/hostsAd
+cat /tmp/someonewhocares.conf /tmp/adaway.conf > /tmp/hostsAd
 echo
 echo -e "\e[1;36m 删除hosts临时文件\e[0m"
-rm -rf /tmp/someonewhocares.conf /tmp/adaway.conf /tmp/mallist
+rm -rf /tmp/someonewhocares.conf /tmp/adaway.conf
 echo
 echo -e "\e[1;36m 删除注释和本地规则\e[0m"
 sed -i '/#/d' /tmp/hostsAd
