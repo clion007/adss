0dd49fa35842eef587164c1d1cef88a324ad54f3
clion007 <clion007@126.com>
2024-05-13 21:03:43 +0800

    update

--- config/backup.sh
+++ config/backup.sh
@@ -3,9 +3,6 @@ CRON_FILE=/etc/crontabs/$USER
 echo -e "\e[1;36m 创建dnsmasq规则与更新脚本存放的文件夹\e[0m"
 echo
 echo -e "\e[1;36m 检测和备份当前dnsmasq配置信息\e[0m"
-if [ ! -d /etc/dnsmasq ]; then
-	mkdir -p /etc/dnsmasq
-fi
 if [ ! -d /etc/dnsmasq.d ]; then
 	mkdir -p /etc/dnsmasq.d
 fi
@@ -14,7 +11,8 @@ if [ -f /etc/dnsmasq.conf ]; then
 		cp -p /etc/dnsmasq.conf /etc/dnsmasq.conf.bak
 	fi	
 else
-	echo "" > /etc/dnsmasq.conf
+	echo "您当前没有安装dnsmasq, 请检查安装后再试" 
+	exit 1
 fi
 if [ -f $CRON_FILE ]; then
 	if [ ! -f $CRON_FILE.bak ]; then
