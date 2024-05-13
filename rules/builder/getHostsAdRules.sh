0e963cfccbe6e348894c62e545e32c4584474400
clion007 <clion007@126.com>
2024-05-13 23:12:59 +0800

    update

--- rules/builder/getHostsAdRules.sh
+++ rules/builder/getHostsAdRules.sh
@@ -2,24 +2,24 @@
 echo -e "\e[1;36m 开始下载Hosts规则\e[0m"
 echo
 echo -e "\e[1;36m 下载someonewhocares缓存\e[0m"
-wget --no-check-certificate -c -q -T 60 -O /tmp/someonewhocares.conf https://someonewhocares.org/hosts/zero/hosts
+wget --no-check-certificate -c -q -T 60 -O /tmp/adss/someonewhocares.conf https://someonewhocares.org/hosts/zero/hosts
 echo
 echo -e "\e[1;36m 下载adaway规则缓存\e[0m"
-wget --no-check-certificate -c -q -T 60 -O /tmp/adaway.conf https://adaway.org/hosts.txt
+wget --no-check-certificate -c -q -T 60 -O /tmp/adss/adaway.conf https://adaway.org/hosts.txt
 echo
 echo -e "\e[1;36m 合并hosts缓存\e[0m"
-cat /tmp/fastgithub.conf /tmp/someonewhocares.conf /tmp/adaway.conf > /tmp/hostsAd
+cat /tmp/adss/someonewhocares.conf /tmp/adss/adaway.conf > /tmp/adss/hostsAd
 echo
 echo -e "\e[1;36m 删除hosts临时文件\e[0m"
-rm -rf /tmp/fastgithub.conf /tmp/someonewhocares.conf /tmp/adaway.conf
+rm -rf /tmp/adss/someonewhocares.conf /tmp/adss/adaway.conf
 echo
 echo -e "\e[1;36m 删除注释和本地规则\e[0m"
-sed -i '/#<localhost/,/#<\/localhost>/d' /tmp/hostsAd
-sed -i '/local/d' /tmp/hostsAd
-sed -i 's/#.*//g' /tmp/hostsAd
-sed -i 's/@.*//g' /tmp/hostsAd
-sed -i '/^$/d' /tmp/hostsAd
+sed -i '/#<localhost/,/#<\/localhost>/d' /tmp/adss/hostsAd
+sed -i '/local/d' /tmp/adss/hostsAd
+sed -i 's/#.*//g' /tmp/adss/hostsAd
+sed -i 's/@.*//g' /tmp/adss/hostsAd
+sed -i '/^$/d' /tmp/adss/hostsAd
 echo
 echo -e "\e[1;36m 统一广告规则格式\e[0m"
-sed -i "s/[ ][ ]*/ /g" /tmp/hostsAd
-sed -i "s/0.0.0.0/127.0.0.1/g" /tmp/hostsAd
+sed -i "s/[ ][ ]*/ /g" /tmp/adss/hostsAd
+sed -i "s/0.0.0.0/127.0.0.1/g" /tmp/adss/hostsAd
