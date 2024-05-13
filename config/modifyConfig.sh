a5ca5e1d1cf5abb8941b6d57d5c692507fe272e6
clion007 <clion007@126.com>
2024-05-14 00:06:39 +0800

    update

--- config/modifyConfig.sh
+++ config/modifyConfig.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
 sleep 3
 grep "dnsmasq.d" /etc/dnsmasq.conf >/dev/null
-if [ ! $? -eq 0 ]; then
+if [ $? -eq 0 ]; then
 	sed  -i 's/\#*conf-dir=\/etc\/dnsmasq.d/conf-dir=\/etc\/dnsmasq.d/g' /etc/dnsmasq.conf
-	echo -e "\e[1;36m 配置dnsmasq\e[0m"
-	echo
-	lanip=`ifconfig|grep Bcast|awk '{print $2}'|tr -d "addr:"| tr '\n' ',' | sed 's/,$//'`
-	lanipv6=`ifconfig -a|grep ::1/64|awk '{print $3}'|tr -d "/64"`
-	echo "
+else
+	echo "conf-dir=/etc/dnsmasq.d" >> /etc/dnsmasq.conf
+fi
+lanip=`ifconfig|grep Bcast|awk '{print $2}'|tr -d "addr:"| tr '\n' ',' | sed 's/,$//'`
+echo -e "\e[1;36m 配置dnsmasq\e[0m"
+echo
+echo "
 # 设定域名解析缓存池大小
 cache-size=1000000
 
-# 添加监听地址" >> /etc/dnsmasq.conf
-	if [ -n $lanipv6 ]; then
-		echo "listen-address=$lanip,$lanipv6,127.0.0.1,::1" >> /etc/dnsmasq.d/adss.conf
-	else
-		echo "listen-address=$lanip,127.0.0.1" >> /etc/dnsmasq.d/adss.conf
-	fi
-	echo "
+# 添加监听地址
+listen-address=$lanip,127.0.0.1
+
 # 指定上游DNS服务器配置文件路径
 resolv-file=/etc/dnsmasq.d/adss/resolv.conf
 
@@ -28,26 +26,21 @@ all-servers
 conf-file=/etc/dnsmasq.d/adss/dnsrules.conf
 
 # 添加额外hosts规则
-addn-hosts=/etc/dnsmasq.d/adss/hostsrules.conf" >> /etc/dnsmasq.d/adss.conf
-else
-	echo "conf-dir=/etc/dnsmasq.d" >> /etc/dnsmasq.conf
-fi
-if [ ! -s /etc/dnsmasq.d/adss/resolv.conf ]; then
-	echo -e "\e[1;36m 创建上游DNS配置文件\e[0m"
-	echo
-	echo "# 上游DNS解析服务器
+addn-hosts=/etc/dnsmasq.d/adss/hostsrules.conf" > /etc/dnsmasq.d/adss.conf
+echo -e "\e[1;36m 创建上游DNS配置文件\e[0m"
+echo
+echo "# 上游DNS解析服务器
 # 如需根据自己的网络环境优化DNS服务器，可用ping或DNSBench测速
 # 选择最快的服务器依次按速度快慢顺序手动改写
 
 # 纯净免费公共DNS查询服务器
-nameserver 119.29.29.29 #DNSPod IPv4
-nameserver 2402:4e00:: #DNSPod IPv6
-nameserver 8.8.8.8 #Google IPv4 DNS
-nameserver 101.102.103.104 #TWNIC DNS IPv4
-nameserver 2001:de4::102 #TWNIC DNS IPv6
-nameserver 168.126.63.1 #韩国 DNS IPv4
-nameserver 205.252.144.228 #香港 DNS IPv4" >> /etc/dnsmasq.d/adss/resolv.conf
-fi
+nameserver 119.29.29.29      #DNSPod IPv4
+nameserver 2402:4e00::       #DNSPod IPv6
+nameserver 8.8.8.8           #Google IPv4 DNS
+nameserver 101.102.103.104   #TWNIC DNS IPv4
+nameserver 2001:de4::102     #TWNIC DNS IPv6
+nameserver 168.126.63.1      #韩国 DNS IPv4
+nameserver 205.252.144.228   #香港 DNS IPv4" > /etc/dnsmasq.d/adss/resolv.conf
 sleep 3
 if [ ! -f /etc/dnsmasq.d/adss/userlist ]; then
 	echo -e "\e[1;36m 创建自定义dnsmasq规则\e[0m"
