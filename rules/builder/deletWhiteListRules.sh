20b3275dd2a50c37d2c15c13696d9685bc59c15f
clion007 <clion007@126.com>
2024-05-13 22:30:22 +0800

    update

--- rules/builder/deletWhiteListRules.sh
+++ rules/builder/deletWhiteListRules.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 echo -e "\e[1;36m 删除白名单及误杀规则，时间较长，请耐心等待。。。\e[0m"
-wget --no-check-certificate -c -q -O /tmp/adwhitelist https://gitcode.net/clion007/adss/raw/master/adwhitelist
-sort /etc/dnsmasq/userwhitelist /tmp/adwhitelist | uniq > /tmp/whitelist
-sed -i "/#/d" /tmp/whitelist
-rm -rf /tmp/adwhitelist
+wget --no-check-certificate -c -q -O /tmp/adss/adwhitelist https://gitcode.net/clion007/adss/raw/master/rules/diy/adwhitelist
+sort /etc/dnsmasq.d/adss/userwhitelist /tmp/adss/adwhitelist | uniq > /tmp/adss/whitelist
+sed -i "/#/d" /tmp/adss/whitelist
+rm -rf /tmp/adss/adwhitelist
 while read -r line
 do
-	if [ -s "/etc/dnsmasq/dnsAd" ]; then 
-		sed -i "/$line/d" /etc/dnsmasq/dnsAd
+	if [ -s "/etc/dnsmasq.d/adss/dnsAd" ]; then 
+		sed -i "/$line/d" /etc/dnsmasq.d/adss/dnsAd
 	fi
-	if [ -s "/tmp/hostsAd" ]; then 
-		sed -i "/$line/d" /tmp/hostsAd
+	if [ -s "/tmp/adss/hostsAd" ]; then 
+		sed -i "/$line/d" /tmp/adss/hostsAd
 	fi
-done < /tmp/whitelist
-rm -rf /tmp/whitelist
+done < /tmp/adss/whitelist
+rm -rf /tmp/adss/whitelist
