ad3942096b44b78c49f3e7dd7c68e1bc484ed402
clion007 <clion007@126.com>
2024-05-13 20:33:22 +0800

    update

--- installer/ad_auto.sh
+++ installer/ad_auto.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 clear
 echo
-if [ ! -s /tmp/copyright.sh ]; then
+if [ ! -s /tmp/adss/copyright.sh ]; then
 	wget --no-check-certificate https://gitcode.net/clion007/adss/raw/master/copyright.sh -qO \
-		/tmp/copyright.sh && chmod 775 /tmp/copyright.sh
+		/tmp/adss/copyright.sh && chmod 775 /tmp/adss/copyright.sh
 fi
-if [ -s "/tmp/copyright.sh" ]; then
-	sh /tmp/copyright.sh
+if [ -s "/tmp/adss/copyright.sh" ]; then
+	sh /tmp/adss/copyright.sh
 else
 	echo
 	echo -e "\e[1;36m  `date +'%Y-%m-%d %H:%M:%S'`: 文件下载异常，放弃安装。\e[0m"
 	echo
-	rm -f /tmp/copyright.sh
+	rm -f /tmp/adss/copyright.sh
 	exit 1
 fi
 echo
@@ -19,40 +19,40 @@ echo -e "\e[1;36m 三秒后开始备份安装前路由器相关配置......\e[0m
 echo
 sleep 3
 wget --no-check-certificate https://gitcode.net/clion007/adss/raw/master/backup.sh -qO \
-	/tmp/backup.sh && chmod 775 /tmp/backup.sh && sh /tmp/backup.sh
-rm -f /tmp/backup.sh
+	/tmp/adss/backup.sh && chmod 775 /tmp/adss/backup.sh && sh /tmp/adss/backup.sh
+rm -f /tmp/adss/backup.sh
 echo
 sleep 3
 wget --no-check-certificate https://gitcode.net/clion007/adss/raw/master/modifyConfig.sh -qO \
-	/tmp/modifyConfig.sh  && chmod 775 /tmp/modifyConfig.sh  && sh /tmp/modifyConfig.sh
+	/tmp/adss/modifyConfig.sh  && chmod 775 /tmp/adss/modifyConfig.sh  && sh /tmp/adss/modifyConfig.sh
 rm -f /tmp/modifyConfig.sh
 wget --no-check-certificate https://gitcode.net/clion007/adss/raw/master/initRulesFile.sh -qO \
-	/tmp/initRulesFile.sh  && chmod 775 /tmp/initRulesFile.sh  && sh /tmp/initRulesFile.sh
-rm -f /tmp/initRulesFile.sh
+	/tmp/adss/initRulesFile.sh  && chmod 775 /tmp/adss/initRulesFile.sh  && sh /tmp/adss/initRulesFile.sh
+rm -f /tmp/adss/initRulesFile.sh
 echo
 wget --no-check-certificate https://gitcode.net/clion007/adss/raw/master/getDnsmasqAdRules.sh -qO \
-	/tmp/getDnsmasqAdRules.sh  && chmod 775 /tmp/getDnsmasqAdRules.sh  && sh /tmp/getDnsmasqAdRules.sh
-rm -f /tmp/getDnsmasqAdRules.sh
+	/tmp/adss/getDnsmasqAdRules.sh  && chmod 775 /tmp/adss/getDnsmasqAdRules.sh  && sh /tmp/adss/getDnsmasqAdRules.sh
+rm -f /tmp/adss/getDnsmasqAdRules.sh
 echo
 wget --no-check-certificate https://gitcode.net/clion007/adss/raw/master/getHostsAdRules.sh -qO \
-	/tmp/getHostsAdRules.sh  && chmod 775 /tmp/getHostsAdRules.sh  && sh /tmp/getHostsAdRules.sh
-rm -f /tmp/getHostsAdRules.sh
+	/tmp/adss/getHostsAdRules.sh  && chmod 775 /tmp/adss/getHostsAdRules.sh  && sh /tmp/adss/getHostsAdRules.sh
+rm -f /tmp/adss/getHostsAdRules.sh
 echo
 wget --no-check-certificate https://gitcode.net/clion007/adss/raw/master/deletWhiteListRules.sh -qO \
-	/tmp/deletWhiteListRules.sh  && chmod 775 /tmp/deletWhiteListRules.sh  && sh /tmp/deletWhiteListRules.sh
-rm -f /tmp/deletWhiteListRules.sh
+	/tmp/adss/deletWhiteListRules.sh  && chmod 775 /tmp/adss/deletWhiteListRules.sh  && sh /tmp/adss/deletWhiteListRules.sh
+rm -f /tmp/adss/deletWhiteListRules.sh
 echo
 echo -e "\e[1;36m 删除dnsmasq,hosts重复规则及临时文件\e[0m"
 echo
-awk '!a[$0]++' /tmp/dnsAd >> /tmp/dnsrules.conf
-awk '!a[$0]++' /tmp/hostsAd >> /tmp/hostsrules.conf
+awk '!a[$0]++' /tmp/adss/dnsAd >> /tmp/adss/dnsrules.conf
+awk '!a[$0]++' /tmp/adss/hostsAd >> /tmp/adss/hostsrules.conf
 echo "
-# Modified DNS end" >> /tmp/dnsrules.conf
+# Modified DNS end" >> /tmp/adss/dnsrules.conf
 echo "
-# 修饰hosts结束" >> /tmp/hostsrules.conf
-mv /tmp/dnsrules.conf /etc/dnsmasq.d/dnsrules.conf
-mv /tmp/hostsrules.conf /etc/dnsmasq/hostsrules.conf
-rm -rf /tmp/dnsAd /tmp/hostsAd
+# 修饰hosts结束" >> /tmp/adss/hostsrules.conf
+mv /tmp/adss/dnsrules.conf /etc/dnsmasq.d/dnsrules.conf
+mv /tmp/adss/hostsrules.conf /etc/dnsmasq.d/hostsrules.conf
+rm -rf /tmp/adss/dnsAd /tmp/adss/hostsAd
 sleep 3
 echo -e "\e[1;36m 重启dnsmasq服务\e[0m"
 echo
@@ -62,21 +62,21 @@ sleep 2
 echo -e "\e[1;36m 获取脚本更新脚本\e[0m"
 echo
 wget --no-check-certificate https://gitcode.net/clion007/adss/raw/master/ad_update.sh -qO \
-	/etc/dnsmasq/ad_update.sh && chmod 755 /etc/dnsmasq/ad_update.sh
+	/etc/dnsmasq.d/ad_update.sh && chmod 755 /etc/dnsmasq.d/ad_update.sh
 echo -e "\e[1;36m 获取规则更新脚本\e[0m"
 echo
 wget --no-check-certificate https://gitcode.net/clion007/adss/raw/master/adrules_update.sh -qO \
-	/etc/dnsmasq/adrules_update.sh && chmod 755 /etc/dnsmasq/adrules_update.sh
+	/etc/dnsmasq.d/adrules_update.sh && chmod 755 /etc/dnsmasq.d/adrules_update.sh
 sleep 3
 wget --no-check-certificate https://gitcode.net/clion007/adss/raw/master/cron.sh -qO \
-	/tmp/cron.sh && chmod 775 /tmp/cron.sh && sh /tmp/cron.sh
-rm -f /tmp/cron.sh
+	/tmp/adss/cron.sh && chmod 775 /tmp/adss/cron.sh && sh /tmp/adss/cron.sh
+rm -f /tmp/adss/cron.sh
 echo -e "\e[1;36m 创建脚本更新检测副本\e[0m"
-if [ -s /tmp/ad_auto.sh ]; then
-	mv -f /tmp/ad_auto.sh /etc/dnsmasq/ad_auto.sh
+if [ -s /tmp/adss/ad_auto.sh ]; then
+	mv -f /tmp/adss/ad_auto.sh /etc/dnsmasq.d/ad_auto.sh
 fi
-if [ -f "/tmp/copyright.sh" ]; then
-	rm -f /tmp/copyright.sh
+if [ -f "/tmp/adss/copyright.sh" ]; then
+	rm -f /tmp/adss/copyright.sh
 fi
 echo
 echo
