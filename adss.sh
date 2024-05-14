d95e188006a55bec780734d1f5ed20c96ec4e735
clion007 <clion007@126.com>
2024-05-14 10:10:21 +0800

    update licenses

--- adss.sh
+++ adss.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-
-function implant_openwrt() {
-
-}
+#
+# Auto DNS Smart Script V4.0
+# Project URL https://github.com/clion007/adss
+# Main Module file
+# Copyright © by Clion Nieh Email: clion007@126.com
+# Licenses in GLP V3.0
+#
 
 function install() {
-
+    
 }
 
 function uninstall() {
+
+}
+
+function implant_to_openwrt() {
 
 }
