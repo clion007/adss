01e45fff2c6ee3af01d86f1108638a0169ebf46f
clion007 <clion007@126.com>
2024-05-14 12:13:05 +0800

    update

--- config/cron.sh
+++ config/cron.sh
@@ -21,7 +21,7 @@ grep "reboot" $CRON_FILE >/dev/null
 if [ ! $? -eq 0 ]; then
 	echo -e "\e[1;36m 设置路由器检测网络不通时重启\e[0m"
 	echo "# 检测网络是否畅通，如果不通则重启路由器
-*/2 * * * * /etc/dnsmasq.d/adss/networkChecker.sh" >> $CRON_FILE
+*/2 * * * * /etc/dnsmasq.d/adss/netcheck.sh" >> $CRON_FILE
 	/etc/init.d/cron reload >/dev/null
 	echo
 	echo -e "\e[1;36m 定时重启任务设定完成\e[0m"
