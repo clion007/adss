0aaeef44ee24432d783e32d7a14b3d26a2220fd3
clion007 <clion007@126.com>
2024-05-14 15:35:11 +0800

    update

--- config/cron.sh
+++ config/cron.sh
@@ -2,23 +2,16 @@
 CRON_FILE=/etc/crontabs/$USER
 grep "adss" $CRON_FILE >/dev/null
 if [ ! $? -eq 0 ]; then
-  echo -e "\e[1;31m 添加规则自动更新计划任务\e[0m"
-  if [ -f /etc/dnsmasq.d/ad_update.sh ]; then
-	echo "# 每天 04:25 更新ADSS规则
+  echo -e "\e[1;31m 添加规则自动更新任务\e[0m"
+  echo "# 每天 04:25 更新ADSS规则
 25 4 * * * sh /etc/dnsmasq.d/adss/update.sh > /dev/null 2>&1" >> $CRON_FILE
-  fi	
-  /etc/init.d/cron reload >/dev/null
   echo
-  echo -e "\e[1;36m 自动更新任务添加完成\e[0m"
-  echo
-fi
-grep "reboot" $CRON_FILE >/dev/null
-if [ ! $? -eq 0 ]; then
-  echo -e "\e[1;36m 设置路由器网络不通时重启\e[0m"
+  echo -e "\e[1;36m 设置网络不通重启任务\e[0m"
   echo "# 检测网络，如果不通重启路由器
 */2 * * * * /etc/dnsmasq.d/adss/netcheck.sh" >> $CRON_FILE
-  /etc/init.d/cron reload >/dev/null
   echo
-  echo -e "\e[1;36m 定时重启任务设定完成\e[0m"
+  echo -e "\e[1;36m 网络不通重启任务设定完成\e[0m"
+  echo
+  /etc/init.d/cron reload >/dev/null
   echo
 fi
