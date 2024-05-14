450abc8545fb32fd4b67d6f633e2027035d80c51
clion007 <clion007@126.com>
2024-05-14 11:11:10 +0800

    update

--- adss.sh
+++ adss.sh
@@ -9,6 +9,14 @@
 
 function install() {
     echo "检测与处理倚赖关系"
+    if [ ! type opkg >/dev/null 2>&1 ]; then
+      echo "本脚本仅支持 Openwrt 系列固件使用，您的当前固件不支持安装"
+      exit 1
+    fi
+    opkg list_installed | grep "dnsmasq" > /dev/null
+    if [ ! $? -eq 0 ]; then
+      opkg install dnsmasq-full
+    fi
 }
 
 function uninstall() {
