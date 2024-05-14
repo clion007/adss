57c0fbc0d93c946761876891dc330c6a649deae0
clion007 <clion007@126.com>
2024-05-14 15:29:16 +0800

    update

--- installer/uninstall.sh
+++ installer/uninstall.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 if [ -f /var/lock/opkg.lock ]; then
-	rm -f /var/lock/opkg.lock
+  rm -f /var/lock/opkg.lock
 fi
 echo -e "\e[1;31m 开始卸载已安装dnsmasq脚本配置 \e[0m"
 echo
 echo -e "\e[1;31m 删除残留文件夹以及配置 \e[0m"
 rm -rf /etc/dnsmasq.d/adss*
 if [ -f /etc/dnsmasq.conf.adss.bak ]; then
-	mv -f /etc/dnsmasq.conf.adss.bak /etc/dnsmasq.conf
+  mv -f /etc/dnsmasq.conf.adss.bak /etc/dnsmasq.conf
 fi
 if [ ! -d /etc/dnsmasq.d.adss.bak ]; then
   mv -f /etc/dnsmasq.d.adss.bak /etc/dnsmasq.d
@@ -15,7 +15,7 @@ fi
 echo
 echo -e "\e[1;31m 删除相关计划任务\e[0m"
 if [ -f /etc/crontabs/$USER.adss.bak ]; then
-	mv -f /etc/crontabs/$USER.adss.bak /etc/crontabs/$USER
+  mv -f /etc/crontabs/$USER.adss.bak /etc/crontabs/$USER
 fi
 /etc/init.d/cron reload > /dev/null 2>&1
 echo
