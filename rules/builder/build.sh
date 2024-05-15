a865f8429666141e1237f431e1f77daeafc2e40e
clion007 <clion007@126.com>
2024-05-15 22:28:37 +0800

    update

--- rules/builder/build.sh
+++ rules/builder/build.sh
@@ -21,8 +21,8 @@ rm -f /tmp/adss/deletWhiteListRules.sh
 echo 
 echo -e "\e[1;36m 删除dnsmasq及hosts重复规则\e[0m"
 echo 
-awk '!a[$0]++' /tmp/adss/dnsAd >> /tmp/adss/dnsrules.conf
-awk '!a[$0]++' /tmp/adss/hostsAd >> /tmp/adss/hostsrules.conf
+awk '!a[$0]++' /tmp/adss/dnsAd >> /tmp/adss/dnsrules.conf 
+awk '!a[$0]++' /tmp/adss/hostsAd >> /tmp/adss/hostsrules.conf 
 echo "
 # Modified DNS end" >> /tmp/adss/dnsrules.conf
 echo "
