ee512717a1288b5b76f0aa970753b028becf86f9
clion007 <clion007@126.com>
2024-05-15 19:13:48 +0800

    update

--- rules/builder/build.sh
+++ rules/builder/build.sh
@@ -1,22 +1,22 @@
  #!/bin/bash
 curl https://gitee.com/clion007/adss/raw/master/rules/builder/initRulesFile.sh -sSo /tmp/adss/initRulesFile.sh
 chmod 775 /tmp/adss/initRulesFile.sh
-sh /tmp/adss/initRulesFile.sh
+bash /tmp/adss/initRulesFile.sh
 rm -f /tmp/adss/initRulesFile.sh
 echo 
 curl https://gitee.com/clion007/adss/raw/master/rules/builder/getDnsmasqAdRules.sh -sSo /tmp/adss/getDnsmasqAdRules.sh
 chmod 775 /tmp/adss/getDnsmasqAdRules.sh
-sh /tmp/adss/getDnsmasqAdRules.sh
+bash /tmp/adss/getDnsmasqAdRules.sh
 rm -f /tmp/adss/getDnsmasqAdRules.sh
 echo 
 curl https://gitee.com/clion007/adss/raw/master/rules/builder/getHostsAdRules.sh -sSo /tmp/adss/getHostsAdRules.sh
 chmod 775 /tmp/adss/getHostsAdRules.sh
-sh /tmp/adss/getHostsAdRules.sh
+bash /tmp/adss/getHostsAdRules.sh
 rm -f /tmp/adss/getHostsAdRules.sh
 echo 
 curl https://gitee.com/clion007/adss/raw/master/rules/builder/deletWhiteListRules.sh -sSo /tmp/adss/deletWhiteListRules.sh
 chmod 775 /tmp/adss/deletWhiteListRules.sh
-sh /tmp/adss/deletWhiteListRules.sh
+bash /tmp/adss/deletWhiteListRules.sh
 rm -f /tmp/adss/deletWhiteListRules.sh
 echo 
 echo -e "\e[1;36m 删除dnsmasq及hosts重复规则\e[0m"
