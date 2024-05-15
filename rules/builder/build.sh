e6fa478b29a59ea466cb9f3ea68d6934ef26fc91
clion007 <clion007@126.com>
2024-05-15 11:47:12 +0800

    update

--- rules/builder/build.sh
+++ rules/builder/build.sh
@@ -1,24 +1,11 @@
  #!/bin/bash
-mkdir -p /tmp/adss
-wget -cqO /tmp/adss/initRulesFile.sh https://gitee.com/clion007/adss/raw/master/rules/builder/initRulesFile.sh
-chmod 775 /tmp/adss/initRulesFile.sh
-. /tmp/adss/initRulesFile.sh
-rm -f /tmp/adss/initRulesFile.sh
+bash <(curl -fsSL  https://gitee.com/clion007/adss/raw/master/rules/builder/initRulesFile.sh)
 echo
-wget -cqO /tmp/adss/getDnsmasqAdRules.sh https://gitee.com/clion007/adss/raw/master/rules/builder/getDnsmasqAdRules.sh
-chmod 775 /tmp/adss/getDnsmasqAdRules.sh
-. /tmp/adss/getDnsmasqAdRules.sh
-rm -f /tmp/adss/getDnsmasqAdRules.sh
+bash <(curl -fsSL  https://gitee.com/clion007/adss/raw/master/rules/builder/getDnsmasqAdRules.sh)
 echo
-wget -cqO /tmp/adss/getHostsAdRules.sh https://gitee.com/clion007/adss/raw/master/rules/builder/getHostsAdRules.sh
-chmod 775 /tmp/adss/getHostsAdRules.sh
-. /tmp/adss/getHostsAdRules.sh
-rm -f /tmp/adss/getHostsAdRules.sh
+bash <(curl -fsSL  https://gitee.com/clion007/adss/raw/master/rules/builder/getHostsAdRules.sh)
 echo
-wget -cqO /tmp/adss/deletWhiteListRules.sh https://gitee.com/clion007/adss/raw/master/rules/builder/deletWhiteListRules.sh
-chmod 775 /tmp/adss/deletWhiteListRules.sh
-. /tmp/adss/deletWhiteListRules.sh
-rm -f /tmp/adss/deletWhiteListRules.sh
+bash <(curl -fsSL  https://gitee.com/clion007/adss/raw/master/rules/builder/deletWhiteListRules.sh)
 echo
 echo -e "\e[1;36m 删除dnsmasq及hosts重复规则及临时文件\e[0m"
 echo
