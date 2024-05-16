0eb3dad693593a0c0eaef783234a467e4791b82a
clion007 <clion007@126.com>
2024-05-16 11:14:13 +0800

    update

--- rules/builder/build.sh
+++ rules/builder/build.sh
@@ -12,8 +12,6 @@ echo -e "\e[1;36m 删除dnsmasq及hosts重复规则\e[0m"
 echo 
 awk '!a[$0]++' /tmp/adss/dnsAd >> /tmp/adss/dnsrules.conf 
 awk '!a[$0]++' /tmp/adss/hostsAd >> /tmp/adss/hostsrules.conf 
-echo "
-# Modified DNS end" >> /tmp/adss/dnsrules.conf 
-echo "
-# 修饰hosts结束" >> /tmp/adss/hostsrules.conf 
+echo "# Modified DNS end" >> /tmp/adss/dnsrules.conf 
+echo "# 修饰hosts结束" >> /tmp/adss/hostsrules.conf 
 rm -f /tmp/adss/*Ad
