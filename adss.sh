ba7dad55d2d23cdae70397c73ec81a8fe24fc421
clion007 <clion007@126.com>
2024-05-16 12:27:29 +0800

    updated by clion007

--- adss.sh
+++ adss.sh
@@ -4,7 +4,7 @@
 # Project URL https://github.com/clion007/adss
 # Main Module file
 # Copyright © by Clion Nieh Email: clion007@126.com
-# Licenses in GPL-3.0
+# Licenses in GPL-3
 #
 
 function show_copyright() {
