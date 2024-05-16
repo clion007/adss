6d627093fd8d2c93fce861cfa6f9d88364fc9afd
clion007 <clion007@126.com>
2024-05-16 14:35:28 +0800

    updated by clion007

--- updater/update.sh
+++ updater/update.sh
@@ -13,26 +13,24 @@ echo
 echo -e "\e[1;36m 开始检测更新脚本及规则\e[0m"
 echo 
 curl https://gitee.com/clion007/adss/raw/master/updater/update.sh -sSo /tmp/adss/update.sh
-chmod 775 /tmp/adss/update.sh
 curl https://gitee.com/clion007/adss/raw/master/updater/rules_update.sh -sSo /tmp/adss/rules_update.sh
-chmod 775 /tmp/adss/rules_update.sh
 if [ -s "/tmp/adss/update.sh" -a -s "/tmp/adss/rules_update.sh" ]; then
-	if ( ! cmp -s /tmp/adss/update.. /usr/share/adss/update.sh ); then
+	if ( ! cmp -s /tmp/adss/update.sh /usr/share/adss/update.sh ); then
 		echo " `date +'%Y-%m-%d %H:%M:%S'`: 检测到新版升级脚本......3秒后即将开始更新！"
 		echo 
 		sleep 3
 		echo -e "\e[1;36m 开始更新升级脚本\e[0m"
-		mv -f /tmp/adss/update.. /usr/share/adss/update.sh
+		mv -f /tmp/adss/update.sh /usr/share/adss/update.sh
 		echo 
 		. /usr/share/adss/update.sh
 		echo " `date +'%Y-%m-%d %H:%M:%S'`: 升级脚本更新完成。"
 		echo 
-	elif ( ! cmp -s /tmp/adss/rules_update.. /usr/share/adss/rules_update.sh ); then
+	elif ( ! cmp -s /tmp/adss/rules_update.sh /usr/share/adss/rules_update.sh ); then
 		echo " `date +'%Y-%m-%d %H:%M:%S'`: 检测到新版规则升级脚本......3秒后即将开始更新！"
 		echo 
 		sleep 3
 		echo -e "\e[1;36m 开始更新规则升级脚本\e[0m"
-		mv -f /tmp/adss/rules_update.. /usr/share/adss/rules_update.sh
+		mv -f /tmp/adss/rules_update.sh /usr/share/adss/rules_update.sh
 		echo 
 		. /usr/share/adss/rules_update.sh
 		echo " `date +'%Y-%m-%d %H:%M:%S'`: 规则升级脚本更新完成。"
