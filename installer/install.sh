686734af6fb76ba56f29fc5283e5bc97c32fcf20
clion007 <clion007@126.com>
2024-05-16 14:29:42 +0800

    updated by clion007

--- installer/install.sh
+++ installer/install.sh
@@ -4,11 +4,9 @@ echo -e "\e[1;36m 开始备份路由器相关配置\e[0m"
 echo 
 curl https://gitee.com/clion007/adss/raw/master/config/backup.sh -sSo /tmp/adss/backup.sh
 . . /tmp/adss/backup.sh
-rm -f /tmp/adss/backup.sh
 echo 
 curl https://gitee.com/clion007/adss/raw/master/config/modifyConfig.sh -sSo /tmp/adss/modifyConfig.sh
 . . /tmp/adss/modifyConfig.sh
-rm -f /tmp/modifyConfig.sh
 echo -e "\e[1;36m 获取规则文件\e[0m"
 echo 
 curl https://gitee.com/clion007/adss/raw/master/rules/file/dnsrules.conf -sSo /usr/share/adss/rules/dnsrules.conf
@@ -19,11 +17,8 @@ killall dnsmasq > /dev/null 2>&1
 /etc/init.d/dnsmasq restart > /dev/null 2>&1
 sleep 2
 curl https://gitee.com/clion007/adss/raw/master/adss.sh -sSo /usr/share/adss/adss.sh
-chmod 755 /usr/share/adss/adss.sh
 curl https://gitee.com/clion007/adss/raw/master/updater/update.sh -sSo /usr/share/adss/update.sh
-chmod 755 /usr/share/adss/update.sh
 curl https://gitee.com/clion007/adss/raw/master/updater/rules_update.sh -sSo /usr/share/adss/rules_update.sh
-chmod 755 /usr/share/adss/rules_update.sh
 sleep 3
 curl https://gitee.com/clion007/adss/raw/master/config/cron.sh -sSo /tmp/adss/cron.sh
 . . /tmp/adss/cron.sh
