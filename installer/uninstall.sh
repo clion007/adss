e53e9ba79f1314c7b464b4c9061793c7a683d66c
clion007 <clion007@126.com>
2024-05-17 22:43:23 +0800

    updated by clion007

--- installer/uninstall.sh
+++ installer/uninstall.sh
@@ -10,17 +10,14 @@ fi
 echo -e "\e[1;31m 开始卸载已安装dnsmasq脚本配置 \e[0m"
 echo 
 echo -e "\e[1;31m 删除残留文件夹以及配置 \e[0m"
-rm -rf /usr/share/adss
-if [ -f /etc/dnsmasq.conf.adss.bak ]; then
-  mv -f /etc/dnsmasq.conf.adss.bak /etc/dnsmasq.conf
-fi
 echo 
+rm -rf /usr/share/adss /etc/dnsmasq.d/*adss* /tmp/dnsmasq.d/*adss*
 echo -e "\e[1;31m 删除相关计划任务\e[0m"
-if [ -f /etc/crontabs/$USER.adss.bak ]; then
-  mv -f /etc/crontabs/$USER.adss.bak /etc/crontabs/$USER
+echo 
+if [ -f /etc/crontabs/$USER-adss.bak ]; then
+  mv -f /etc/crontabs/$USER-adss.bak /etc/crontabs/$USER
 fi
 /etc/init.d/cron reload > /dev/null 2>&1
-echo 
 echo -e "\e[1;31m 重启dnsmasq服务\e[0m"
 killall dnsmasq > /dev/null 2>&1
 /etc/init.d/dnsmasq restart > /dev/null 2>&1
