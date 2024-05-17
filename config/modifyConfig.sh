045e4abe10bda1eb5fa3e4b565ab78ddb45a6928
clion007 <clion007@126.com>
2024-05-17 13:22:36 +0800

    updated by clion007

--- config/modifyConfig.sh
+++ config/modifyConfig.sh
@@ -29,13 +29,13 @@ echo "# 上游DNS解析服务器
 # 选择最快的服务器依次按速度快慢顺序手动改写
 
 # 纯净免费公共DNS查询服务器
-nameserver 119.29.29.29      #DNSPod IPv4
-nameserver 2402:4e00::       #DNSPod IPv6
-nameserver 8.8.8.8           #Google IPv4 DNS
-nameserver 101.102.103.104   #TWNIC DNS IPv4
-nameserver 2001:de4::102     #TWNIC DNS IPv6
-nameserver 168.126.63.1      #韩国 DNS IPv4
-nameserver 205.252.144.228   #香港 DNS IPv4" > /usr/share/adss/resolv.conf 
+nameserver 119.29.29.29      # DNSPod IPv4
+nameserver 2402:4e00::       # DNSPod IPv6
+# nameserver 8.8.8.8           # Google IPv4 DNS
+nameserver 101.102.103.104   # TWNIC DNS IPv4
+nameserver 2001:de4::102     # TWNIC DNS IPv6
+nameserver 168.126.63.1      # 韩国 DNS IPv4
+nameserver 205.252.144.228   # 香港 DNS IPv4" > /usr/share/adss/resolv.conf 
 sleep 3
 if [ ! -f /usr/share/adss/rules/rules/userlist ]; then
 	echo -e "\e[1;36m 创建自定义dnsmasq规则\e[0m"
