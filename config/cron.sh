b6ae577910990fbe3e7973c491bcfdee3eea573a
clion007 <clion007@126.com>
2024-05-24 17:08:20 +0800

    fix network check error

--- config/cron.sh
+++ config/cron.sh
@@ -7,8 +7,8 @@ if [ ! $? -eq 0 ]; then
 25 4 * * * sh /usr/share/adss/update.sh > /dev/null 2>&1" >> $CRON_FILE
   echo 
   echo -e "\e[1;36m 设置网络不通重启任务\e[0m"
-  echo "# 检测网络，如果不通重启路由器
-*/2 * * * * bash /usr/share/adss/netcheck.sh" >> $CRON_FILE
+  echo "# 每天 04:55 检测网络，如果不通重启路由器
+55 4 * * * ping -c2 -w5 baidu.com || touch /etc/banner && reboot" >> $CRON_FILE
   /etc/init.d/cron reload >/dev/null
   echo 
 fi
