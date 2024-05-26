363f1d024587bac442479f1c13880105e3796b7b
clion007 <clion007@126.com>
2024-05-26 17:05:49 +0800

    Updated by Clion Nieh

--- installer/install.sh
+++ installer/install.sh
@@ -37,6 +37,8 @@ curl https://gitee.com/clion007/adss/raw/master/config/configer.sh -sLSo /tmp/ad
 sh /tmp/adss/configer.sh
 echo -e "\e[1;36m 获取规则文件\e[0m"
 echo 
+curl https://gitee.com/clion007/adss/raw/master/installer/adss -sLSo /etc/init.d/adss
+chmod +x /etc/init.d/adss
 curl https://raw.gitmirror.com/clion007/adss/master/rules/file/dnsrules.conf -sLSo /etc/dnsmasq.d/adss/rules/dnsrules.conf
 curl https://raw.gitmirror.com/clion007/adss/master/rules/file/hostsrules.conf -sLSo /etc/dnsmasq.d/adss/rules/hostsrules.conf
 curl https://gitee.com/clion007/adss/raw/master/adss.sh -sLSo /usr/share/adss/adss.sh
