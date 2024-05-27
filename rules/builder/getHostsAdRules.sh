382b90aafce69047002baa7b647ddbed51a0633a
clion007 <clion007@126.com>
2024-05-27 17:33:37 +0800

    Updated by Clion Nieh

--- rules/builder/getHostsAdRules.sh
+++ rules/builder/getHostsAdRules.sh
@@ -4,11 +4,14 @@ echo
 echo -e "\e[1;36m 下载someonewhocares缓存\e[0m"
 curl https://someonewhocares.org/hosts/zero/hosts -sLSo /tmp/adss/someonewhocares.conf
 echo 
+echo -e "\e[1;36m 下载大圣净化缓存\e[0m"
+curl https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts -sLSo /tmp/adss/adwars.conf
+echo
 echo -e "\e[1;36m 下载adaway规则缓存\e[0m"
 curl https://adaway.org/hosts.txt -sLSo /tmp/adss/adaway.conf
 echo 
 echo -e "\e[1;36m 合并hosts缓存\e[0m"
-cat /tmp/adss/someonewhocares.conf /tmp/adss/adaway.conf > /tmp/adss/hostsAd 
+cat /tmp/adss/someonewhocares.conf /tmp/adss/adwars.conf /tmp/adss/adaway.conf > /tmp/adss/hostsAd 
 echo 
 echo -e "\e[1;36m 删除hosts临时文件\e[0m"
 rm -rf /tmp/adss/someonewhocares.conf /tmp/adss/adaway.conf
