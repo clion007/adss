0e82d461b715453dac0fad889348f15f8b7aaab0
clion007 <clion007@126.com>
2024-06-16 18:30:56 +0800

    Update configer.sh

--- config/configer.sh
+++ config/configer.sh
@@ -50,4 +50,6 @@ fi
 
 echo -e "\e[1;31m 添加配置目录权限 \e[0m"
 echo 
-sed -i 's/$resolvdir $user_dhcpscript/$resolvdir $user_dhcpscript \/etc\/dnsmasq.d\//g' /etc/init.d/dnsmasq
+if [ `grep -c "\/etc\/dnsmasq.d\/" /etc/init.d/dnsmasq` -ne '0' ];then
+	sed -i 's/$resolvdir $user_dhcpscript/$resolvdir $user_dhcpscript \/etc\/dnsmasq.d\//g' /etc/init.d/dnsmasq
+fi
