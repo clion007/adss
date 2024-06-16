ce9e43a53536b2e2ef1a55e08bb5a947fffebe8e
clion007 <clion007@126.com>
2024-06-16 18:11:33 +0800

    Update prepare.sh

--- config/prepare.sh
+++ config/prepare.sh
@@ -3,12 +3,8 @@ CRON_FILE=/etc/crontabs/$USER
 echo -e "\e[1;36m 创建dnsmasq规则与更新脚本存放的文件夹\e[0m"
 echo 
 echo -e "\e[1;36m 检测和备份当前dnsmasq配置信息\e[0m"
-if [ ! -d /etc/dnsmasq.d/adss/rules ]; then
-  mkdir -p /etc/dnsmasq.d/adss/rules
-fi
-if [ ! -d /usr/share/adss ]; then
-  mkdir -p /usr/share/adss
-fi
+mkdir -p /etc/dnsmasq.d/adss/rules
+mkdir -p /usr/share/adss
 touch $CRON_FILE
 if [ ! -f $CRON_FILE-adss.bak ]; then
   cp -p $CRON_FILE $CRON_FILE-adss.bak
