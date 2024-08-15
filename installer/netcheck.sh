5278783d3612205a2ea50f645e20b9e3b02af6d1
clion007 <clion007@126.com>
2024-08-15 10:25:25 +0800

    Update netcheck.sh

--- installer/netcheck.sh
+++ installer/netcheck.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
-
-# 检测网络连接
-ping -c 2 -w 5 baidu.com > /dev/null 2>&1
-if [ $? -ne 0 ];then
-    # 检测到网络异常，重启网络服务
-    /etc/init.d/network restart
-    ping -c 2 -w 5 baidu.com > /dev/null 2>&1
-    if [ $? -ne 0 ];then
-        # 重启网络服务后，检测到网络仍然异常，重启路由器
-        reboot
-    else
-        exit 0
-    fi
+rm -rf /tmp/baidu.html > /dev/null 2>&1
+curl --connect-timeout 9 -o /tmp/baidu.html -s -w %{time_namelookup}: https://www.baidu.com > /dev/null 2>&1
+if [[ -f /tmp/baidu.html ]] && [[ `grep -c "百度一下" /tmp/baidu.html` -ge '1' ]]; then
+  rm -rf /tmp/baidu.html
 else
-    exit 0
+  wangluo1='1'
+  rm -rf /tmp/baidu.html
 fi
+
+rm -rf /tmp/sogou.html > /dev/null 2>&1
+curl --connect-timeout 9 -o /tmp/sogou.html -s -w %{time_namelookup}: https://www.sogou.com > /dev/null 2>&1
+if [[ -f /tmp/sogou.html ]] && [[ `grep -c "搜狗搜索" /tmp/sogou.html` -ge '1' ]]; then
+  rm -rf /tmp/sogou.html
+else
+  wangluo2='2'
+  rm -rf /tmp/sogou.html
+fi
+if [[ "${wangluo1}" == "1" && "${wangluo2}" == "2" ]]; then
+  ifdown wan
+  sleep 1
+  ifup wan
+fi
+exit 0
