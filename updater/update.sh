77a353ae8280899c861970015936e2c66ea03eac
clion007 <clion007@126.com>
2024-08-27 16:29:03 +0800

    Update update.sh

--- updater/update.sh
+++ updater/update.sh
@@ -4,7 +4,7 @@ curl https://gitee.com/clion007/adss/raw/master/installer/copyright.sh -sLSo /tm
 if [ -s "/tmp/adss/copyright.sh" ]; then
   sh /tmp/adss/copyright.sh
 else
-  echo -e "\e[1;36m  `date +'%Y-%m-%d %H:%M:%S'`: 网络异常，退出更新。\e[0m"
+  echo -e "\e[1;36m `date +'%Y-%m-%d %H:%M:%S'`: 网络异常，退出更新。\e[0m"
   echo 
   exit 1
 fi
@@ -14,32 +14,32 @@ curl https://gitee.com/clion007/adss/raw/master/updater/update.sh -sLSo /tmp/ads
 curl https://gitee.com/clion007/adss/raw/master/updater/rules_update.sh -sLSo /tmp/adss/rules_update.sh
 if [ -s "/tmp/adss/update.sh" -a -s "/tmp/adss/rules_update.sh" ]; then
 	if ( ! cmp -s /tmp/adss/update.sh /usr/share/adss/update.sh ); then
-		echo " `date +'%Y-%m-%d %H:%M:%S'`: 检测到新版升级脚本......开始更新！"
+		echo "\e[1;36m `date +'%Y-%m-%d %H:%M:%S'`: 检测到新版升级脚本......开始更新！\e[0m"
 		echo 
 		echo -e "\e[1;36m 开始更新升级脚本\e[0m"
 		mv -f /tmp/adss/update.sh /usr/share/adss/update.sh
 		echo 
 		sh /usr/share/adss/update.sh
-		echo " `date +'%Y-%m-%d %H:%M:%S'`: 升级脚本更新完成。"
+		echo "\e[1;36m `date +'%Y-%m-%d %H:%M:%S'`: 升级脚本更新完成。\e[0m"
 		echo 
 	elif ( ! cmp -s /tmp/adss/rules_update.sh /usr/share/adss/rules_update.sh ); then
-		echo " `date +'%Y-%m-%d %H:%M:%S'`: 检测到新版规则升级脚本......开始更新！"
+		echo "\e[1;36m `date +'%Y-%m-%d %H:%M:%S'`: 检测到新版规则升级脚本......开始更新！\e[0m"
 		echo 
 		echo -e "\e[1;36m 开始更新规则升级脚本\e[0m"
 		mv -f /tmp/adss/rules_update.sh /usr/share/adss/rules_update.sh
 		echo 
 		sh /usr/share/adss/rules_update.sh
-		echo " `date +'%Y-%m-%d %H:%M:%S'`: 规则升级脚本更新完成。"
+		echo "\e[1;36m `date +'%Y-%m-%d %H:%M:%S'`: 规则升级脚本更新完成。\e[0m"
 		echo 
 	else
-		echo " `date +'%Y-%m-%d %H:%M:%S'`: 脚本已为最新，开始检测规则更新"
+		echo "\e[1;36m `date +'%Y-%m-%d %H:%M:%S'`: 脚本已为最新，开始检测规则更新。\e[0m"
 		echo 
 		sh /usr/share/adss/rules_update.sh
-		echo " `date +'%Y-%m-%d %H:%M:%S'`: 规则已经更新完成。"
+		echo "\e[1;36m `date +'%Y-%m-%d %H:%M:%S'`: 规则已经更新完成。\e[0m"
 		echo 
 	fi
 else
-	echo " `date +'%Y-%m-%d %H:%M:%S'`: 脚本文件下载异常，放弃本次更新。"
+	echo "\e[1;36m `date +'%Y-%m-%d %H:%M:%S'`: 脚本文件下载异常，放弃本次更新。\e[0m"
 	echo 
 	rm -rf /tmp/adss
 	exit 1;
