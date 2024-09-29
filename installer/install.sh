71c9cbe29fa4a875375e5a7ef54c112ae1e0014c
clion007 <clion007@126.com>
2024-09-29 11:19:51 +0800

    Update curl options

--- installer/install.sh
+++ installer/install.sh
@@ -42,8 +42,8 @@ chmod 755 /etc/init.d/adss
 if [ -f /etc/rc.d/S90adss ]; then
   ln -s /etc/init.d/adss /etc/rc.d/S90adss
 fi
-curl https://raw.gitmirror.com/clion007/adss/master/rules/file/dnsrules.conf -sLSo /etc/dnsmasq.d/adss/rules/dnsrules.conf
-curl https://raw.gitmirror.com/clion007/adss/master/rules/file/hostsrules.conf -sLSo /etc/dnsmasq.d/adss/rules/hostsrules.conf
+curl https://raw.gitmirror.com/clion007/adss/master/rules/file/dnsrules.conf -sLCSo /etc/dnsmasq.d/adss/rules/dnsrules.conf
+curl https://raw.gitmirror.com/clion007/adss/master/rules/file/hostsrules.conf -sLCSo /etc/dnsmasq.d/adss/rules/hostsrules.conf
 curl https://gitee.com/clion007/adss/raw/master/adss.sh -sLSo /usr/share/adss/adss.sh
 curl https://gitee.com/clion007/adss/raw/master/installer/netcheck.sh -sLSo /usr/share/adss/netcheck.sh
 curl https://gitee.com/clion007/adss/raw/master/updater/update.sh -sLSo /usr/share/adss/update.sh
