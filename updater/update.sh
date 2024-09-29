f9924bcd472b81df2b97cd683bd011025d8f44ec
clion007 <clion007@126.com>
2024-09-29 11:56:16 +0800

    Update by Clion

--- updater/update.sh
+++ updater/update.sh
@@ -18,23 +18,35 @@ if [ -s "/tmp/adss/update.sh" -a -s "/tmp/adss/rules_update.sh" ]; then
 		echo 
 		echo -e "\e[1;36m 开始更新升级脚本\e[0m"
 		mv -f /tmp/adss/update.sh /usr/share/adss/update.sh
+		chmod 755 /usr/share/adss/update.sh
 		echo 
-		sh /usr/share/adss/update.sh
-		echo -e "\e[1;36m `date +'%Y-%m-%d %H:%M:%S'`: 升级脚本更新完成。\e[0m"
+		echo -e "\e[1;36m `date +'%Y-%m-%d %H:%M:%S'`: 升级脚本完成。\e[0m"
 		echo 
+		if ( ! cmp -s /tmp/adss/rules_update.sh /usr/share/adss/rules_update.sh ); then
+			echo -e "\e[1;36m `date +'%Y-%m-%d %H:%M:%S'`: 检测到新版规则升级脚本......开始更新！\e[0m"
+			echo 
+			echo -e "\e[1;36m 开始更新规则升级脚本\e[0m"
+			mv -f /tmp/adss/rules_update.sh /usr/share/adss/rules_update.sh
+			chmod 755 /usr/share/adss/rules_update.sh
+			echo 
+			/usr/share/adss/rules_update.sh
+			echo -e "\e[1;36m `date +'%Y-%m-%d %H:%M:%S'`: 规则升级脚本更新完成。\e[0m"
+			echo 
+		fi
 	elif ( ! cmp -s /tmp/adss/rules_update.sh /usr/share/adss/rules_update.sh ); then
 		echo -e "\e[1;36m `date +'%Y-%m-%d %H:%M:%S'`: 检测到新版规则升级脚本......开始更新！\e[0m"
 		echo 
 		echo -e "\e[1;36m 开始更新规则升级脚本\e[0m"
 		mv -f /tmp/adss/rules_update.sh /usr/share/adss/rules_update.sh
+		chmod 755 /usr/share/adss/rules_update.sh
 		echo 
-		sh /usr/share/adss/rules_update.sh
+		/usr/share/adss/rules_update.sh
 		echo -e "\e[1;36m `date +'%Y-%m-%d %H:%M:%S'`: 规则升级脚本更新完成。\e[0m"
 		echo 
 	else
 		echo -e "\e[1;36m `date +'%Y-%m-%d %H:%M:%S'`: 脚本已为最新，开始检测规则更新。\e[0m"
 		echo 
-		sh /usr/share/adss/rules_update.sh
+		/usr/share/adss/rules_update.sh
 		echo -e "\e[1;36m `date +'%Y-%m-%d %H:%M:%S'`: 规则已经更新完成。\e[0m"
 		echo 
 	fi
