7578869ee5c9160bc42be834d2f9ddff2efb543d
clion007 <clion007@126.com>
2024-10-08 21:42:03 +0800

    规则生成细节优化

--- updater/rules_update.sh
+++ updater/rules_update.sh
@@ -12,19 +12,22 @@ echo
 echo -e "\e[1;36m 添加用户定义规则\e[0m"
 cat /etc/dnsmasq.d/adss/rules/userlist > /tmp/adss/userlist 
 sed -i "/#/d" /tmp/adss/userlist
-sed -i '/^$/d' /tmp/adss/userlist # 删除空行
+sed -i '/^\s*$/d' /tmp/adss/userlist # 删除空行
 echo 
-echo -e "\e[1;36m 生成用户定义黑名单规则\e[0m"
+echo -e "\e[1;36m 生成用户黑名单规则\e[0m"
 cat /etc/dnsmasq.d/adss/rules/userblacklist > /tmp/adss/blacklist
+sed -i "/#/d" /tmp/adss/blacklist # 删除注释
+sed -i '/^\s*$/d' /tmp/adss/blacklist # 删除空行
 sed -i '/./{s|^|address=/|;s|$|/127.0.0.1|}' /tmp/adss/blacklist # 生成黑名单规则，支持通配符
 echo 
 echo -e "\e[1;36m 合并处理规则\e[0m"
 cat /tmp/adss/blacklist >> /tmp/adss/dnsrules
 sed -i '/localhost/d' /tmp/adss/dnsrules # 删除本地规则
 sed -i 's/#.*//g' /tmp/adss/dnsrules # 删除注释内容
-sed -i '/^$/d' /tmp/adss/dnsrules # 删除空行
+sed -i '/^\s*$/d' /tmp/adss/dnsrules # 删除空行
+sed -i '/\/$/d' /tmp/adss/dnsrules.conf # 删除dns空规则
 echo 
-echo -e "\e[1;36m 删除用户定义白名单中包含规则\e[0m"
+echo -e "\e[1;36m 删除用户白名单中包含规则\e[0m"
 cat /etc/dnsmasq.d/adss/rules/userwhitelist | uniq > /tmp/adss/whitelist 
 sed -i "/#/d" /tmp/adss/whitelist
 while read -r line
