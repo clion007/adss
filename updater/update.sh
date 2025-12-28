6dde2fa7611eea23de3b167925124eb94a8b1787
clion007 <clion007@126.com>
2025-12-28 18:06:40 +0800

    Update update.sh to enhance update script messages

--- updater/update.sh
+++ updater/update.sh
@@ -22,6 +22,8 @@ if [ -s "/tmp/adss/update.sh" -a -s "/tmp/adss/rules_update.sh" ]; then
 		echo 
 		echo -e "\e[1;36m `date +'%Y-%m-%d %H:%M:%S'`: 升级脚本完成。\e[0m"
 		echo 
+		echo -e "\e[1;36m `date +'%Y-%m-%d %H:%M:%S'`: 检测规则升级脚本是否需要更新。\e[0m"
+		echo 
 		if ( ! cmp -s /tmp/adss/rules_update.sh /usr/share/adss/rules_update.sh ); then
 			echo -e "\e[1;36m `date +'%Y-%m-%d %H:%M:%S'`: 检测到新版规则升级脚本......开始更新。\e[0m"
 			echo 
@@ -34,7 +36,7 @@ if [ -s "/tmp/adss/update.sh" -a -s "/tmp/adss/rules_update.sh" ]; then
 			/usr/share/adss/rules_update.sh
 			echo -e "\e[1;36m `date +'%Y-%m-%d %H:%M:%S'`: 规则更新已完成。\e[0m"
 		else
-			echo -e "\e[1;36m `date +'%Y-%m-%d %H:%M:%S'`: 开始检测规则更新。\e[0m"
+			echo -e "\e[1;36m `date +'%Y-%m-%d %H:%M:%S'`: 规则升级脚本无需更新，开始检测规则更新。\e[0m"
 			echo 
 			/usr/share/adss/rules_update.sh
 			echo -e "\e[1;36m `date +'%Y-%m-%d %H:%M:%S'`: 规则更新已完成。\e[0m"
