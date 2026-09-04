bf90076a75360fffc18a93d27912de16b52de550
clion007 <clion007@126.com>
2026-09-04 11:36:29 +0800

    fix(config/cron.sh): 修改定时任务执行命令为adss upgrade
    将原update.sh调用替换为官方推荐的adss upgrade命令，统一任务执行方式

--- config/cron.sh
+++ config/cron.sh
@@ -7,7 +7,7 @@ grep "adss" $CRON_FILE > /dev/null
 if [ $? -ne 0 ]; then
   message r "添加规则自动更新任务"
   echo "# 每天 04:25 更新ADSS规则
-25 4 * * * /usr/share/adss/update.sh > /dev/null 2>&1" >> $CRON_FILE
+25 4 * * * adss upgrade > /dev/null 2>&1" >> $CRON_FILE
   message l "设置网络不通重启任务"
   echo "# 每5分钟检测一次网络，网络异常，重启网络服务
 # */5 * * * * /usr/share/adss/netcheck.sh > /dev/null 2>&1" >> $CRON_FILE
